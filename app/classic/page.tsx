61c629c90a00fe8026e47c0bd9ad12fdd4cc3291
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-27 16:01:34 -0400

    creates pages for sub routes

---
+++ app/classic/page.tsx
@@ -0,0 +1,11 @@
+import OptionsBar from "@/components/OptionsBar";
+import React from "react";
+
+export default function classic() {
+  return (
+    <div>
+      <OptionsBar />
+      classic
+    </div>
+  );
+}
---
+++ app/move/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function move() {
+  return <div>move</div>;
+}
