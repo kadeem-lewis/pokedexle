8303eeb55c40f9e49f88c467a331a0e6dc25ffce
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-27 16:03:09 -0400

    separated navbar into two components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,9 @@
 import React from "react";
-import { AiFillSetting, AiOutlineQuestionCircle } from "react-icons/ai";
-import { IoIosStats } from "react-icons/io";
 
-export const Navbar = () => {
+export default function Navbar() {
   return (
-    <header className=" max-w-sm md:max-w-full flex justify-between">
-      <nav>
-        <button>
-          <AiOutlineQuestionCircle />
-        </button>
-      </nav>
-      <h1>Pokedle</h1>
-      <nav>
-        <button>
-          <AiFillSetting />
-        </button>
-        <button>
-          <IoIosStats />
-        </button>
-      </nav>
+    <header className=" max-w-sm md:max-w-full flex justify-center">
+      <h1 className="text-4xl">Pokedle</h1>
     </header>
   );
-};
+}
