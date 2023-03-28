af3141de56e16dd0ac6cac876df941acd2032285
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-28 01:07:48 -0400

    added theme switch to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import ThemeSwitch from "./ThemeSwitch";
 export default function Navbar() {
   return (
-    <header className=" max-w-sm md:max-w-full flex justify-center">
+    <header className=" max-w-sm md:max-w-full flex justify-between items-center">
       <h1 className="text-4xl">Pokedle</h1>
+      <div>
+        <ThemeSwitch />
+      </div>
     </header>
   );
 }
