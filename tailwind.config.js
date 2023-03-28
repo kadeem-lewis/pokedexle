d7094e0d0c79a14aee8e5459d990cbfe20ca04c1
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-28 01:08:03 -0400

    extended various tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "pokemon-ds": ["var(--font-pokemon-ds)"],
+      },
+      boxShadow: {
+        "border-1": "0 0 0 4px aqua, 0 0 0 10px black",
+        "border-2": "0 0 0 4px red, 0 0 0 10px blue",
+        "border-3": "0 0 0 4px red, 0 0 0 10px blue",
+      },
+    },
   },
+
+  darkMode: "class",
   plugins: [],
 };
