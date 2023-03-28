c0f76f38f77b03c9a1719ef6a6ddcca3fecb8e58
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-28 03:12:20 -0400

    created navigation array to pass into GameButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
-import PageCard from "@/components/PageCard";
+import GameButton from "@/components/GameButton";
 import Link from "next/link";
-
+const NAV_LINKS = [
+  {
+    title: "Classic",
+    description: "Guess the Pokemon",
+    path: "/classic",
+    icon: "",
+  },
+  {
+    title: "Move",
+    description: "Guess the Move",
+    path: "/move",
+    icon: "",
+  },
+];
 export default function Home() {
   return (
     <div>
       {/* Need to create pages array and map them to page card */}
-      <Link href="/classic">
-        <PageCard />
-      </Link>
-      <PageCard />
+      <h3 className="text-2xl">Test your Pokemon Knowledge</h3>
+      {NAV_LINKS.map((link) => (
+        <Link href={link.path} key={link.title}>
+          <GameButton title={link.title} description={link.description} />
+        </Link>
+      ))}
     </div>
   );
 }
