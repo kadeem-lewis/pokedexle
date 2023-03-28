e1e0b5e250b1172bd25911aee4259b145661b2f9
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-28 03:15:48 -0400

    implemented structure for  headlessui components I will need in project

--- app/classic/page.tsx
+++ app/classic/page.tsx
@@ -1,11 +1,39 @@
+"use client";
 import OptionsBar from "@/components/OptionsBar";
-import React from "react";
+import React, { useState, Fragment } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { FaCaretRight } from "react-icons/fa";
+const people = [
+  "Durward Reynolds",
+  "Kenton Towne",
+  "Therese Wunsch",
+  "Benedict Kessler",
+  "Katelyn Rohan",
+];
 
-export default function classic() {
+export default function Classic() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.toLowerCase().includes(query.toLowerCase());
+        });
   return (
     <div>
       <OptionsBar />
-      classic
+      <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+        <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+        <Combobox.Options>
+          {filteredPeople.map((person) => (
+            <Combobox.Option key={person} value={person}>
+              {person}
+            </Combobox.Option>
+          ))}
+        </Combobox.Options>
+      </Combobox>
     </div>
   );
 }
--- app/move/page.tsx
+++ app/move/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 import OptionsBar from "@/components/OptionsBar";
 import React from "react";
-import { Combobox } from "@headlessui/react";
+import { Combobox, Tab } from "@headlessui/react";
 
-export default function move() {
+export default function Move() {
   return (
     <div>
       <OptionsBar />
       move
+      <Tab.Group>
+        <Tab.List>
+          <Tab>Daily</Tab>
+          <Tab>Unlimited</Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>hi</Tab.Panel>
+          <Tab.Panel>hello</Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
       <Combobox>
         <Combobox.Input />
         <Combobox.Options></Combobox.Options>
