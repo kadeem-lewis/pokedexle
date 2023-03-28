a9f32a65a8ef88eb49500336e0b38ea94e6c4d63
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-28 03:13:59 -0400

    attempted to get site branding to use custom font

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ThemeSwitch from "./ThemeSwitch";
+import Link from "next/link";
 export default function Navbar() {
   return (
     <header className=" max-w-sm md:max-w-full flex justify-between items-center">
-      <h1 className="text-4xl">Pokedle</h1>
+      <h1 className="text-4xl">
+        <Link href="/" className=" font-pokemon-solid">
+          Pokedle
+        </Link>
+      </h1>
       <div>
         <ThemeSwitch />
       </div>
