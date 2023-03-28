8d4792e5317e0b09ce86bad972708a9d8ddcbe1a
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-28 01:07:03 -0400

    became working on the main page for the pokemon moves game

--- app/move/page.tsx
+++ app/move/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+import OptionsBar from "@/components/OptionsBar";
 import React from "react";
+import { Combobox } from "@headlessui/react";
 
 export default function move() {
-  return <div>move</div>;
+  return (
+    <div>
+      <OptionsBar />
+      move
+      <Combobox>
+        <Combobox.Input />
+        <Combobox.Options></Combobox.Options>
+      </Combobox>
+    </div>
+  );
 }
