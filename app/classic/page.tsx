cacd0c4d97feea7db24f5c0296f5df7658a7339d
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-29 01:03:36 -0400

    removed example combobox details

--- app/classic/page.tsx
+++ app/classic/page.tsx
@@ -2,35 +2,15 @@
 import OptionsBar from "@/components/layout/OptionsBar";
 import React, { useState, Fragment } from "react";
 import { Combobox, Transition } from "@headlessui/react";
-const people = [
-  "Durward Reynolds",
-  "Kenton Towne",
-  "Therese Wunsch",
-  "Benedict Kessler",
-  "Katelyn Rohan",
-];
 
 export default function Classic() {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
-  const [query, setQuery] = useState("");
-
-  const filteredPeople =
-    query === ""
-      ? people
-      : people.filter((person) => {
-          return person.toLowerCase().includes(query.toLowerCase());
-        });
   return (
     <div>
       <OptionsBar />
-      <Combobox value={selectedPerson} onChange={setSelectedPerson}>
-        <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+      <Combobox>
+        <Combobox.Input />
         <Combobox.Options>
-          {filteredPeople.map((person) => (
-            <Combobox.Option key={person} value={person}>
-              {person}
-            </Combobox.Option>
-          ))}
+          <Combobox.Option value={"hi"}>hi</Combobox.Option>
         </Combobox.Options>
       </Combobox>
     </div>
