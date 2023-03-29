9e077f5df394aaba386c5b53e17284e233e68d00
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-29 17:11:16 -0400

    created content components to pass into modal

---
+++ components/content/AboutContent.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutContent() {
+  return <div>AboutContent</div>;
+}
---
+++ components/content/StatsContent.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function StatsContent() {
+  return <div>StatsContent</div>;
+}
