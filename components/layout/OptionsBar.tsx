3b1979776b3ac533c44297c24a4d13289e3a9a2d
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-30 17:22:18 -0400

    created tooltips for all buttons in icon bar

--- components/layout/OptionsBar.tsx
+++ components/layout/OptionsBar.tsx
@@ -5,7 +5,7 @@ import {
   ChartBarIcon,
   ExclamationCircleIcon,
 } from "@heroicons/react/24/solid";
-import OptionsModal from "../modals/OptionsModal";
+import OptionsModal from "../ui/OptionsModal";
 import SettingsContent from "../content/SettingsContent";
 import AboutContent from "../content/AboutContent";
 import StatsContent from "../content/StatsContent";
@@ -17,8 +17,14 @@ export default function OptionsBar() {
   const [howToPlayClick, setHowToPlayClick] = useState(false);
   return (
     <nav className=" border-b-2 border-current flex justify-around py-2">
-      <button onClick={() => setAboutClick(true)}>
+      <button onClick={() => setAboutClick(true)} className="has-tooltip">
         <QuestionMarkCircleIcon className="h-6 w-6 text-current" />
+        <span
+          role="tooltip"
+          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+        >
+          How to Play
+        </span>
       </button>
       {aboutClick && (
         <OptionsModal
@@ -29,8 +35,14 @@ export default function OptionsBar() {
           <AboutContent />
         </OptionsModal>
       )}
-      <button onClick={() => setSettingClick(true)}>
+      <button onClick={() => setSettingClick(true)} className="has-tooltip">
         <Cog6ToothIcon className="h-6 w-6 text-current" />
+        <span
+          role="tooltip"
+          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+        >
+          Settings
+        </span>
         {settingClick && (
           <OptionsModal
             isOpen={settingClick}
@@ -41,8 +53,14 @@ export default function OptionsBar() {
           </OptionsModal>
         )}
       </button>
-      <button onClick={() => setStatsClick(true)}>
+      <button onClick={() => setStatsClick(true)} className="has-tooltip">
         <ChartBarIcon className="h-6 w-6 text-current" />
+        <span
+          role="tooltip"
+          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+        >
+          Stats
+        </span>
       </button>
       {statsClick && (
         <OptionsModal
@@ -53,8 +71,14 @@ export default function OptionsBar() {
           <StatsContent />
         </OptionsModal>
       )}
-      <button onClick={() => setHowToPlayClick(true)}>
+      <button onClick={() => setHowToPlayClick(true)} className="has-tooltip">
         <ExclamationCircleIcon className="h-6 w-6 text-current" />
+        <span
+          role="tooltip"
+          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+        >
+          About
+        </span>
       </button>
       {howToPlayClick && (
         <OptionsModal
