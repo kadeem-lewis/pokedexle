d34d9a2572023b06fd44348511463bda1b933d70
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-30 18:02:04 -0400

    added aria described by to all buttons with tooltips

--- components/layout/OptionsBar.tsx
+++ components/layout/OptionsBar.tsx
@@ -17,11 +17,15 @@ export default function OptionsBar() {
   const [howToPlayClick, setHowToPlayClick] = useState(false);
   return (
     <nav className=" border-b-2 border-current flex justify-around py-2">
-      <button onClick={() => setAboutClick(true)} className="has-tooltip">
+      <button
+        onClick={() => setAboutClick(true)}
+        aria-describedby="how-to-play-desc"
+      >
         <QuestionMarkCircleIcon className="h-6 w-6 text-current" />
         <span
           role="tooltip"
-          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+          id="how-to-play-desc"
+          className="px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
         >
           How to Play
         </span>
@@ -36,11 +40,15 @@ export default function OptionsBar() {
         </OptionsModal>
       )}
 
-      <button onClick={() => setSettingClick(true)} className="has-tooltip">
+      <button
+        onClick={() => setSettingClick(true)}
+        aria-describedby="setting-desc"
+      >
         <Cog6ToothIcon className="h-6 w-6 text-current" />
         <span
           role="tooltip"
-          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+          id="setting-desc"
+          className="px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
         >
           Settings
         </span>
@@ -54,11 +62,12 @@ export default function OptionsBar() {
           </OptionsModal>
         )}
       </button>
-      <button onClick={() => setStatsClick(true)} className="has-tooltip">
+      <button onClick={() => setStatsClick(true)} aria-describedby="stats-desc">
         <ChartBarIcon className="h-6 w-6 text-current" />
         <span
           role="tooltip"
-          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+          id="stats-desc"
+          className="px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
         >
           Stats
         </span>
@@ -72,11 +81,12 @@ export default function OptionsBar() {
           <StatsContent />
         </OptionsModal>
       )}
-      <button onClick={() => setHowToPlayClick(true)} className="has-tooltip">
+      <button onClick={() => setAboutClick(true)} aria-describedby="about-desc">
         <ExclamationCircleIcon className="h-6 w-6 text-current" />
         <span
           role="tooltip"
-          className="tooltip px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
+          id="about-desc"
+          className="px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
         >
           About
         </span>
