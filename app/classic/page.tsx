dd0da302cb26e5020f4d71e21c455a231bed076d
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-30 17:13:28 -0400

    deleted sample data and unused  fetch function

--- app/classic/page.tsx
+++ app/classic/page.tsx
@@ -3,19 +3,6 @@ import MyComboBox from "@/components/MyComboBox";
 import OptionsBar from "@/components/layout/OptionsBar";
 import { useState, useEffect } from "react";
 
-const myArray = [
-  { name: "John", url: "https://example.com/John" },
-  { name: "Jane", url: "https://example.com/Jane" },
-  { name: "Mike", url: "https://example.com/Mike" },
-  { name: "Sara", url: "https://example.com/Sara" },
-  { name: "Tom", url: "https://example.com/Tom" },
-  { name: "Alice", url: "https://example.com/Alice" },
-  { name: "Bob", url: "https://example.com/Bob" },
-];
-async function getPokemon() {
-  const res = await fetch("https://pokeapi.co/api/v2/move?limit=10000");
-  return res.json();
-}
 export default function Classic() {
   const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(false);
