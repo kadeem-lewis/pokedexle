f94129e647ed4618b23dcbc6fed897f50fc3d8e6
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-30 01:32:39 -0400

    move and classic page now make calls to the pokeapi to fill combobox with possible options

--- app/classic/page.tsx
+++ app/classic/page.tsx
@@ -1,18 +1,51 @@
 "use client";
+import MyComboBox from "@/components/MyComboBox";
 import OptionsBar from "@/components/layout/OptionsBar";
-import React, { useState, Fragment } from "react";
-import { Combobox, Transition } from "@headlessui/react";
+import { useState, useEffect } from "react";
 
+const myArray = [
+  { name: "John", url: "https://example.com/John" },
+  { name: "Jane", url: "https://example.com/Jane" },
+  { name: "Mike", url: "https://example.com/Mike" },
+  { name: "Sara", url: "https://example.com/Sara" },
+  { name: "Tom", url: "https://example.com/Tom" },
+  { name: "Alice", url: "https://example.com/Alice" },
+  { name: "Bob", url: "https://example.com/Bob" },
+];
+async function getPokemon() {
+  const res = await fetch("https://pokeapi.co/api/v2/move?limit=10000");
+  return res.json();
+}
 export default function Classic() {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+        );
+        const data = await response.json();
+        setData(data.results);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
   return (
     <div>
       <OptionsBar />
-      <Combobox>
-        <Combobox.Input />
-        <Combobox.Options>
-          <Combobox.Option value={"hi"}>hi</Combobox.Option>
-        </Combobox.Options>
-      </Combobox>
+      <MyComboBox data={data} />
     </div>
   );
 }
--- app/move/page.tsx
+++ app/move/page.tsx
@@ -1,27 +1,48 @@
 "use client";
 import OptionsBar from "@/components/layout/OptionsBar";
-import React from "react";
-import { Combobox, Tab } from "@headlessui/react";
+import MyComboBox from "@/components/MyComboBox";
+import React, { useState, useEffect } from "react";
+
+interface Item {
+  name: string;
+  url: string;
+}
 
 export default function Move() {
+  const [data, setData] = useState<Array<Item>>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/move?limit=100000&offset=0"
+        );
+        const data = await response.json();
+        const uniqueData = Array.from(
+          new Set(data.results.map((item: Item) => item.name))
+        ).map((name) => {
+          return data.results.find((item: Item) => item.name === name);
+        });
+        setData(uniqueData);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
   return (
     <div>
       <OptionsBar />
-      move
-      <Tab.Group>
-        <Tab.List>
-          <Tab>Daily</Tab>
-          <Tab>Unlimited</Tab>
-        </Tab.List>
-        <Tab.Panels>
-          <Tab.Panel>hi</Tab.Panel>
-          <Tab.Panel>hello</Tab.Panel>
-        </Tab.Panels>
-      </Tab.Group>
-      <Combobox>
-        <Combobox.Input />
-        <Combobox.Options></Combobox.Options>
-      </Combobox>
+      <MyComboBox data={data} />
     </div>
   );
 }
