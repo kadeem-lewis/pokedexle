4f804a40018b0c6dad9918a4560a67b567b6891e
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-31 00:57:01 -0400

    replaced pokedle text with temporary pokedle logo

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ThemeSwitch from "../buttons/ThemeSwitch";
 import Link from "next/link";
+import Image from "next/image";
 export default function Navbar() {
   return (
     <header className=" max-w-sm md:max-w-full flex justify-between items-center">
       <h1 className="text-4xl">
-        <Link href="/">Pokedle</Link>
+        <Link href="/">
+          <Image src="./logo.svg" alt="logo image" width={150} height={150} />
+        </Link>
       </h1>
       <div>
         <ThemeSwitch />
