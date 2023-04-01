fd44b97db7ccd033b89658350872ee37422356a9
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-31 23:46:46 -0400

    removed use of tooltip until a better solution is found

--- components/layout/OptionsBar.tsx
+++ components/layout/OptionsBar.tsx
@@ -10,7 +10,6 @@ import SettingsContent from "../content/SettingsContent";
 import AboutContent from "../content/AboutContent";
 import StatsContent from "../content/StatsContent";
 import HowToPlayContent from "../content/HowToPlayContent";
-import * as Tooltip from "@radix-ui/react-tooltip";
 
 export default function OptionsBar() {
   const [settingClick, setSettingClick] = useState(false);
@@ -19,23 +18,9 @@ export default function OptionsBar() {
   const [howToPlayClick, setHowToPlayClick] = useState(false);
   return (
     <nav className=" border-b-2 border-current flex justify-around py-2">
-      <Tooltip.Provider>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild>
-            <button
-              onClick={() => setAboutClick(true)}
-              aria-describedby="how-to-play-desc"
-            >
-              <QuestionMarkCircleIcon className="h-6 w-6 text-current" />
-            </button>
-          </Tooltip.Trigger>
-          <Tooltip.Portal>
-            <Tooltip.Content className=" rounded py-2 px-4 transition-all bg-white dark:bg-black">
-              How to Play
-            </Tooltip.Content>
-          </Tooltip.Portal>
-        </Tooltip.Root>
-      </Tooltip.Provider>
+      <button onClick={() => setAboutClick(true)}>
+        <QuestionMarkCircleIcon className="h-6 w-6 text-current" />
+      </button>
       {howToPlayClick && (
         <OptionsModal
           isOpen={howToPlayClick}
@@ -46,18 +31,8 @@ export default function OptionsBar() {
         </OptionsModal>
       )}
 
-      <button
-        onClick={() => setSettingClick(true)}
-        aria-describedby="setting-desc"
-      >
+      <button onClick={() => setSettingClick(true)}>
         <Cog6ToothIcon className="h-6 w-6 text-current" />
-        <span
-          role="tooltip"
-          id="setting-desc"
-          className="px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
-        >
-          Settings
-        </span>
         {settingClick && (
           <OptionsModal
             isOpen={settingClick}
@@ -68,15 +43,8 @@ export default function OptionsBar() {
           </OptionsModal>
         )}
       </button>
-      <button onClick={() => setStatsClick(true)} aria-describedby="stats-desc">
+      <button onClick={() => setStatsClick(true)}>
         <ChartBarIcon className="h-6 w-6 text-current" />
-        <span
-          role="tooltip"
-          id="stats-desc"
-          className="px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
-        >
-          Stats
-        </span>
       </button>
       {statsClick && (
         <OptionsModal
@@ -87,15 +55,8 @@ export default function OptionsBar() {
           <StatsContent />
         </OptionsModal>
       )}
-      <button onClick={() => setAboutClick(true)} aria-describedby="about-desc">
+      <button onClick={() => setAboutClick(true)}>
         <ExclamationCircleIcon className="h-6 w-6 text-current" />
-        <span
-          role="tooltip"
-          id="about-desc"
-          className="px-3 py-2 text-center bg-white dark:bg-black rounded relative border-2 border-current"
-        >
-          About
-        </span>
       </button>
       {aboutClick && (
         <OptionsModal
