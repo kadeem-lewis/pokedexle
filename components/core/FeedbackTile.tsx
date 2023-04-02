a09324f3842643fce3abc90cf4582f1785b52ada
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-02 03:28:21 -0400

    created a component that will handle logic for display if the guess was correct or not

---
+++ components/core/FeedbackTile.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function FeedbackTile() {
+  //generates a new grid item with if the item was correct, higher or lower than the correct value
+  return <div>FeedbackTile</div>;
+}
