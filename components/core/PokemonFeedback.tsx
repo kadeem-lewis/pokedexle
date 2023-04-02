36a2e08680ad7a2e8b4498fb7091eff8d99e5f21
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-02 03:43:16 -0400

    created component to handle game feedback

--- components/core/PokemonFeedback.tsx
+++ components/core/PokemonFeedback.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAtom } from "jotai";
+import { guessAtom, itemAtom } from "./Gamebox";
+import { guessedAnswerAtom } from "../ui/MyComboBox";
 
 export default function PokemonFeedback() {
+  const [item] = useAtom(itemAtom);
+  const [guesses, setGuesses] = useAtom(guessAtom);
+  const [guessedAnswer] = useAtom(guessedAnswerAtom);
+  console.log(guessedAnswer);
+
+  useEffect(() => {
+    const getItem = async () => {
+      const res = await fetch(guessedAnswer);
+    };
+  });
   return <div>PokemonFeedback</div>;
 }
