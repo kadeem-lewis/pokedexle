d583b885dad804bff008a61af4de9280143b6095
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 00:39:39 -0400

    converted gamebox into a server side component that fetches pokemon data

--- src/components/core/Gamebox.tsx
+++ src/components/core/Gamebox.tsx
@@ -1,46 +1,39 @@
-"use client";
-//Try to convert this to a server component
-import React, { useEffect } from "react";
-import { useAtom } from "jotai";
-import { itemAtom, guessAtom, Pokemon } from "@/stores/Store";
-import { Item } from "@/stores/Store";
+import { Item, Pokemon } from "@/stores/Store";
 import PokemonFeedback from "./PokemonFeedback";
 
-export default function Gamebox({ itemArray }: { itemArray: Array<Item> }) {
-  const [item, setItem] = useAtom(itemAtom);
-  const [guesses, setGuesses] = useAtom(guessAtom);
-  useEffect(() => {
-    const itemNumber = Math.floor(Math.random() * itemArray.length) + 1;
-    const chosenItem = itemArray[itemNumber];
-    async function getItem() {
-      try {
-        const res = await fetch(chosenItem.url);
-        const data = await res.json();
-        const res2 = await fetch(data.species.url);
-        const data2 = await res2.json();
-        const generation = data2.generation.name;
-        const filteredData: Pokemon = {
-          name: data.name,
-          types: data.types,
-          weight: data.weight,
-          height: data.height,
-          generation,
-          sprite: data.sprites.front_default,
-        };
-        setItem(filteredData);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    getItem();
-  }, [itemArray, setItem]);
+async function getPokemonStats(url: string): Promise<Pokemon> {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const res2 = await fetch(data.species.url);
+    const data2 = await res2.json();
+    const generation = data2.generation.name;
+    return {
+      name: data.name,
+      types: data.types,
+      weight: data.weight,
+      height: data.height,
+      generation,
+      sprite: data.sprites.front_default,
+    };
+  } catch (e) {
+    throw e;
+  }
+}
+export default async function Gamebox({ itemArray }: { itemArray: Item[] }) {
+  // const [item, setItem] = useAtom(itemAtom);
+  // const [guesses, setGuesses] = useAtom(guessAtom);
+
+  const itemNumber = Math.floor(Math.random() * itemArray.length) + 1;
+  const chosenItem = itemArray[itemNumber];
+  const chosenItemData: Pokemon = await getPokemonStats(chosenItem.url);
+
   //TODO function to deal with game being over is added here
 
   //TODO add modal to show when game is over
-  console.log("Gamebox Rerendered");
   return (
     <div>
-      <PokemonFeedback />
+      <PokemonFeedback correctAnswer={chosenItemData} />
     </div>
   );
 }
