33b19ae18f83f906cd493bd41f62c64f33e7c734
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 00:41:32 -0400

    component now takes correct answer as a prop and updates based on chances to guessed answer

--- src/components/core/PokemonFeedback.tsx
+++ src/components/core/PokemonFeedback.tsx
@@ -1,21 +1,20 @@
+"use client";
 import React, { useEffect } from "react";
 import { useAtom } from "jotai";
-import { guessAtom, itemAtom } from "@/stores/Store";
+import { guessAtom, itemAtom, Pokemon } from "@/stores/Store";
 import { guessedAnswerAtom } from "../ui/MyComboBox";
 
-export default function PokemonFeedback() {
-  const [item] = useAtom(itemAtom);
+export default function PokemonFeedback({
+  correctAnswer,
+}: {
+  correctAnswer: Pokemon;
+}) {
+  const [item, setItem] = useAtom(itemAtom);
   const [guesses, setGuesses] = useAtom(guessAtom);
   const [guessedAnswer, setGuessedAnswer] = useAtom(guessedAnswerAtom);
   console.log("Feedback rerendered");
   console.log(guessedAnswer);
 
-  useEffect(() => {
-    const getItem = async () => {
-      const res = await fetch(guessedAnswer.url);
-      const data = await res.json();
-      setGuessedAnswer(data);
-    };
-  }, [guessedAnswer, setGuessedAnswer]);
+  useEffect(() => {}, [guessedAnswer, setGuessedAnswer]);
   return <div>PokemonFeedback</div>;
 }
