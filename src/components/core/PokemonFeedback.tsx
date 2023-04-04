940ee609e76de081ade5b09da38a8568fb4399ed
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 02:53:45 -0400

    mapped array guesses to component that checks if value was correct or not

--- src/components/core/PokemonFeedback.tsx
+++ src/components/core/PokemonFeedback.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useEffect } from "react";
-import { useAtom } from "jotai";
-import { guessAtom, itemAtom, Pokemon } from "@/stores/Store";
-import { guessedAnswerAtom } from "../ui/MyComboBox";
+import { useAtom, useAtomValue } from "jotai";
+import { guessAtom, guessedItemsAtom, itemAtom, Pokemon } from "@/stores/Store";
+import FeedbackTile from "./FeedbackTile";
 
 export default function PokemonFeedback({
   correctAnswer,
@@ -10,11 +10,29 @@ export default function PokemonFeedback({
   correctAnswer: Pokemon;
 }) {
   const [item, setItem] = useAtom(itemAtom);
-  const [guesses, setGuesses] = useAtom(guessAtom);
-  const [guessedAnswer, setGuessedAnswer] = useAtom(guessedAnswerAtom);
-  console.log("Feedback rerendered");
-  console.log(guessedAnswer);
 
-  useEffect(() => {}, [guessedAnswer, setGuessedAnswer]);
-  return <div>PokemonFeedback</div>;
+  const [guesses, setGuesses] = useAtom(guessAtom);
+  const guessedItems = useAtomValue(guessedItemsAtom);
+  console.log("Feedback rerendered");
+  console.log(correctAnswer);
+  console.log(guessedItems);
+
+  const feedbackStatements = guessedItems.map((guessedItem) => (
+    <FeedbackTile
+      guessedItem={guessedItem}
+      key={guessedItem.name}
+      correctItem={correctAnswer}
+    />
+  ));
+  return (
+    <div className="grid grid-cols-6">
+      <div>Name</div>
+      <div>Type 1</div>
+      <div>Type 2</div>
+      <div>Generation</div>
+      <div>Weight</div>
+      <div>Height</div>
+      {feedbackStatements}
+    </div>
+  );
 }
