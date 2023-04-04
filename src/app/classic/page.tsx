5b65b4ad2a2c253e0f86fcdc887d5fd20c5c6f11
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 00:37:57 -0400

    creating api request to fetch pokemonList on server

--- src/app/classic/page.tsx
+++ src/app/classic/page.tsx
@@ -1,34 +1,27 @@
-"use client";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
 import MyComboBox from "@/components/ui/MyComboBox";
 import OptionsBar from "@/components/layout/OptionsBar";
 import Gamebox from "@/components/core/Gamebox";
 import { Item } from "@/stores/Store";
+async function getPokemonList() {
+  try {
+    const res = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    );
+    const pokemonData = await res.json();
+    return pokemonData.results;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-function Classic({
-  pokemonList,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log(pokemonList);
+export default async function Classic() {
+  const pokemonList = await getPokemonList();
   return (
     <div>
       <OptionsBar />
+      {/* @ts-expect-error Async Server Component */}
       <Gamebox itemArray={pokemonList} />
       <MyComboBox data={pokemonList} />
     </div>
   );
 }
-const getStaticProps: GetStaticProps<{ pokemonList: Item[] }> = async (
-  context
-) => {
-  const res = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-  );
-  const pokemonData = await res.json();
-  const pokemonList: Item[] = await pokemonData.results;
-  return {
-    props: {
-      pokemonList,
-    },
-  };
-};
-export default Classic;
