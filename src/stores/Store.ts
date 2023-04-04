41203c47efe3ff6331ca3e6f0a95d89aaff2eb44
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 00:40:33 -0400

    began creating atoms for other states

--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -10,8 +10,16 @@ export interface Pokemon {
   generation: string;
   sprite: string;
 }
-
+function addGuessedAnswer(items: Item[], guessedItem: Item) {}
 import { atom } from "jotai";
+export const gameOver = atom(false);
 export const guessAtom = atom(8);
 export const itemAtom = atom<Pokemon | null>(null);
 export const guessedItemsAtom = atom<Pokemon[]>([]);
+export const addGuessedItem = atom(null, (get, set) => {});
+/*
+when user submits an answer clear the selected value,
+add the value to the guessedAnswer array and then run
+a function to get the api data of the pokemon and 
+check if the stats were correct or not
+*/
