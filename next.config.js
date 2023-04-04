751c47cfbddd3cc06b2a2af5621460f37aef8a38
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 02:50:49 -0400

    added github image path to allow sprites to render using next image

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+const { hostname } = require("os");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/PokeAPI/sprites/master/sprites/pokemon/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
