e43470f87db6118e0535d6d59aa5791a55549061
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 20:09:18 -0400

    switched function to using a graphql query to rest api to get all required data in a single request

--- src/app/classic/page.tsx
+++ src/app/classic/page.tsx
@@ -1,21 +1,45 @@
+import { cache } from "react";
 import MyComboBox from "@/components/ui/MyComboBox";
 import OptionsBar from "@/components/layout/OptionsBar";
 import Gamebox from "@/components/core/Gamebox";
-import { Item } from "@/stores/Store";
-async function getPokemonList() {
+import { updatePokemonData } from "@/helpers/DataProcessing";
+import { Pokemon } from "@/stores/Store";
+
+const query = `
+query MyQuery {
+  pokemon_v2_pokemon {
+    name
+    height
+    weight
+    pokemon_v2_pokemontypes {
+      pokemon_v2_type {
+        name
+      }
+    }
+    pokemon_v2_pokemonspecy {
+      generation_id
+    }
+  }
+}`;
+
+const getPokemonList = cache(async () => {
   try {
-    const res = await fetch(
-      "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-    );
+    const res = await fetch("https://beta.pokeapi.co/graphql/v1beta", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
     const pokemonData = await res.json();
-    return pokemonData.results;
+    return updatePokemonData(pokemonData.data.pokemon_v2_pokemon);
   } catch (err) {
     console.error(err);
   }
-}
+});
 
 export default async function Classic() {
-  const pokemonList = await getPokemonList();
+  const pokemonList: Pokemon[] = (await getPokemonList()) || [];
   return (
     <div>
       <OptionsBar />
