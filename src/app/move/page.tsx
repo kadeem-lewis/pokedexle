f47dc982e54f51c51f0c366b77b92ea713f23f32
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-05 00:21:47 -0400

    created graphql query to get required move data

--- src/app/move/page.tsx
+++ src/app/move/page.tsx
@@ -1,24 +1,47 @@
+import { cache } from "react";
 import OptionsBar from "@/components/layout/OptionsBar";
 import MyComboBox from "@/components/ui/MyComboBox";
+import { updateMoveData } from "@/helpers/DataProcessing";
 
 import { Item } from "@/atoms/GameAtoms";
-async function getMoveList() {
+const query = `
+query MyQuery {
+  pokemon_v2_move {
+    name
+    generation_id
+    pokemon_v2_movedamageclass {
+      name
+    }
+    power
+    pp
+    accuracy
+    pokemon_v2_type {
+      name
+    }
+  }
+}
+`;
+const getMoveList = cache(async () => {
   try {
-    const res = await fetch(
-      "https://pokeapi.co/api/v2/move?limit=100000&offset=0"
-    );
+    const res = await fetch("https://beta.pokeapi.co/graphql/v1beta", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
     const moveData = await res.json();
-    return moveData.results;
+    return updateMoveData(moveData.data.pokemon_v2_move);
   } catch (err) {
     console.error(err);
   }
-}
+});
 export default async function Move() {
   const moveList = await getMoveList();
   return (
     <div>
       <OptionsBar />
-      <MyComboBox data={moveList} />
+      {/* <MyComboBox data={moveList} /> */}
     </div>
   );
 }
