dc180eac7a900676eb659a0a780eaeb4b96e771f
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 23:35:56 -0400

    added comment about missing functionality

--- src/components/core/Gamebox.tsx
+++ src/components/core/Gamebox.tsx
@@ -4,7 +4,7 @@ import PokemonFeedback from "./PokemonFeedback";
 export default async function Gamebox({ itemArray }: { itemArray: Pokemon[] }) {
   const itemNumber = Math.floor(Math.random() * itemArray.length) + 1;
   const chosenItem = itemArray[itemNumber];
-
+  //TODO add functionality to start a new game
   //TODO function to deal with game being over is added here
 
   //TODO add modal to show when game is over
