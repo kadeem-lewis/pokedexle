8f14fb6bef765ae1a2047f0e9e9893b20c558ed0
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-04 20:12:29 -0400

    feedback boxes show mostly accurate feedback and change color based on if guess is correct

--- src/components/core/FeedbackTile.tsx
+++ src/components/core/FeedbackTile.tsx
@@ -2,34 +2,56 @@ import { Item, Pokemon } from "@/stores/Store";
 import React from "react";
 import Image from "next/image";
 interface Props {
-  guessedItem: Item;
+  guessedItem: Pokemon;
   correctItem: Pokemon;
 }
 export default function FeedbackTile({ guessedItem, correctItem }: Props) {
   //generates a new grid item with if the item was correct, higher or lower than the correct value
   return (
     <>
-      <div className="border-2 border-current p-2">{guessedItem.name}</div>
-      <div className="border-2 border-current p-2">
-        {10 <= correctItem.height ? "higher" : "lower"}
-      </div>
-      <div className="border-2 border-current p-2">
-        {(correctItem.types[0] = "fire" ? "X" : "O")}
-      </div>
-      <div className="border-2 border-current p-2">
-        {(correctItem.types[0] = "water" ? "X" : "O")}
-      </div>
-      <div className="border-2 border-current p-2">
-        {(correctItem.generation = "1" ? "OG" : "L")}
-      </div>
-      <div className="border-2 border-current p-2">
-        <Image
-          src={correctItem.sprite}
-          height={64}
-          width={64}
-          alt={`${correctItem.name} sprite`}
-        />
-      </div>
+      {guessedItem.name === correctItem.name ? (
+        <>
+          <div className="border-2 border-current p-2 bg-green-400">
+            {correctItem.name}
+          </div>
+          <div className="border-2 border-current p-2 bg-green-400">
+            {correctItem.generation}
+          </div>
+          <div className="border-2 border-current p-2 bg-green-400">
+            {correctItem.types[0]}
+          </div>
+          <div className="border-2 border-current p-2 bg-green-400">
+            {correctItem.types[1] !== null ? correctItem.types[1] : "none"}
+          </div>
+          <div className="border-2 border-current p-2 bg-green-400">
+            {correctItem.weight}
+          </div>
+          <div className="border-2 border-current p-2 bg-green-400">
+            {correctItem.height}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="border-2 border-current p-2">{guessedItem.name}</div>
+          <div className="border-2 border-current p-2">
+            {correctItem.generation > guessedItem.generation
+              ? "Higher"
+              : "Lower"}
+          </div>
+          <div className="border-2 border-current p-2">
+            {(correctItem.types[0] = "fire" ? "X" : "O")}
+          </div>
+          <div className="border-2 border-current p-2">
+            {(correctItem.types[0] = "water" ? "X" : "O")}
+          </div>
+          <div className="border-2 border-current p-2">
+            {correctItem.weight < guessedItem.weight ? "Higher" : "Lower"}
+          </div>
+          <div className="border-2 border-current p-2">
+            {correctItem.height < guessedItem.height ? "Higher" : "Lower"}
+          </div>
+        </>
+      )}
     </>
   );
 }
