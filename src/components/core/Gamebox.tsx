9ab97ea37cf38549d15e70b6a4d78d82b3f9811b
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-07 02:41:52 -0400

    get a random pokemon everytime a new game starts

--- src/components/core/Gamebox.tsx
+++ src/components/core/Gamebox.tsx
@@ -1,11 +1,24 @@
-import { Pokemon } from "@/atoms/GameAtoms";
+"use client";
+import { Pokemon, newGameAtom } from "@/atoms/GameAtoms";
 import PokemonFeedback from "./PokemonFeedback";
+import { useEffect, useState } from "react";
+import { useSetAtom } from "jotai";
 
-export default async function Gamebox({ itemArray }: { itemArray: Pokemon[] }) {
-  const itemNumber = Math.floor(Math.random() * itemArray.length) + 1;
-  const chosenItem = itemArray[itemNumber];
+function chooseRandomItem(itemArray: Pokemon[]): Pokemon {
+  const itemNumber = Math.floor(Math.random() * itemArray.length);
+  return itemArray[itemNumber];
+}
+
+export default function Gamebox({ itemArray }: { itemArray: Pokemon[] }) {
+  const newGame = useSetAtom(newGameAtom);
+  const [chosenItem, setChosenItem] = useState(() =>
+    chooseRandomItem(itemArray)
+  );
 
   //TODO add functionality to start a new game
+  useEffect(() => {
+    setChosenItem(chooseRandomItem(itemArray));
+  }, [newGame, itemArray]);
   //TODO function to deal with game being over is added here
 
   //TODO add modal to show when game is over
