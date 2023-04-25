ddf328e6b4bd7f75bc94a9ca5dfc7f381474f864
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-24 20:11:43 -0400

    updated navbar width to match other content

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Navbar() {
   return (
-    <header className=" flex max-w-sm items-center justify-between md:max-w-full">
+    <header className=" flex max-w-md items-center justify-between md:max-w-full">
       <h1 className="text-4xl">
         <Link href="/">
           <Image src="./logo.svg" alt="logo image" width={150} height={150} />
