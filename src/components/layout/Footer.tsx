e3b2440bc4ca42cd7e9d3cf400c1e5e5ccf2a364
kadeem-lewis <print.helloworld@genericindividual.com>
2023-05-18 03:12:01 -0400

    updated styling of footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="text-2xl">&#169; 2023 All Rights Reserved</div>;
+  return (
+    <p className="my-4 text-center text-2xl">&#169; 2023 All Rights Reserved</p>
+  );
 }
