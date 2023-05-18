4ae3a11989d56ee4b7f123ab8311941d67f4e369
kadeem-lewis <print.helloworld@genericindividual.com>
2023-05-18 03:18:25 -0400

    reverted options model to headlessui

--- src/components/ui/OptionsModal.tsx
+++ src/components/ui/OptionsModal.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, Fragment } from "react";
-import * as Dialog from "@radix-ui/react-dialog";
+import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 interface Props {
   isOpen: boolean;
@@ -14,20 +14,52 @@ export default function OptionsModal({
   title,
 }: Props) {
   return (
-    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 rounded-lg border-4 border-current bg-white p-4 dark:bg-black">
-          <Dialog.Title className="text-4xl font-bold">{title}</Dialog.Title>
-          <Dialog.DialogDescription></Dialog.DialogDescription>
-          <div>{children}</div>
-          <Dialog.DialogClose>
-            <button aria-label="Close" className=" hover:outline-1">
-              <XMarkIcon className="absolute right-6 top-6 h-6 w-6 stroke-2 text-current" />
-            </button>
-          </Dialog.DialogClose>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-start justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="relative my-8 w-full max-w-md rounded-lg border-4 border-current bg-white p-4 dark:bg-black">
+                <Dialog.Title as="h3" className="text-4xl font-bold">
+                  {title}
+                </Dialog.Title>
+                <button
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  <XMarkIcon className="absolute right-6 top-6 h-6 w-6 stroke-2 text-current" />
+                </button>
+                <div>{children}</div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
