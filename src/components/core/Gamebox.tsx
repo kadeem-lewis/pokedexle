8c605167142c4a26a601e88fb5e9c959056a60d4
kadeem-lewis <print.helloworld@genericindividual.com>
2023-05-22 07:13:10 -0400

    attempted to create tabs for different modes linked to url params

--- src/components/core/Gamebox.tsx
+++ src/components/core/Gamebox.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Tab } from "@headlessui/react";
 import { Pokemon, newGameAtom } from "@/atoms/GameAtoms";
 import PokemonFeedback from "./PokemonFeedback";
 import { useSetAtom } from "jotai";
+import Link from "next/link";
 
 function chooseRandomItem(itemArray: Pokemon[]): Pokemon {
   const itemNumber = Math.floor(Math.random() * itemArray.length);
@@ -15,17 +16,43 @@ export default function Gamebox({ itemArray }: { itemArray: Pokemon[] }) {
   const [chosenItem, setChosenItem] = useState(() =>
     chooseRandomItem(itemArray)
   );
-
+  const [selectedIndex, setSelectedIndex] = useState(0);
   //TODO add functionality to start a new game
   useEffect(() => {
     setChosenItem(chooseRandomItem(itemArray));
   }, [newGame, itemArray]);
 
   return (
-    <Tab.Group>
-      <Tab.List>
-        <Tab>Daily</Tab>
-        <Tab>Unlimited</Tab>
+    <Tab.Group
+      selectedIndex={selectedIndex}
+      onChange={(index) => {
+        setSelectedIndex(index);
+        //If index = 1 then add unlimited param to url
+      }}
+    >
+      <Tab.List className="flex justify-center gap-2">
+        <Tab as={Link} href={"/classic"}>
+          {({ selected }) => (
+            <button
+              className={
+                selected ? "bg-blue-500 text-white" : "bg-white text-black"
+              }
+            >
+              Daily
+            </button>
+          )}
+        </Tab>
+        <Tab as={Link} href={`/classic/?unlimited`} shallow>
+          {({ selected }) => (
+            <button
+              className={
+                selected ? "bg-blue-500 text-white" : "bg-white text-black"
+              }
+            >
+              Unlimited
+            </button>
+          )}
+        </Tab>
       </Tab.List>
       <Tab.Panels>
         <Tab.Panel>In Progress</Tab.Panel>
