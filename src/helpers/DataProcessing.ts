4f181f6078e328d7bfb47040935e7f586355b004
kadeem-lewis <print.helloworld@genericindividual.com>
2023-05-22 01:26:25 -0400

    deleted unused functions

--- src/helpers/DataProcessing.ts
+++ src/helpers/DataProcessing.ts
@@ -1,48 +1,6 @@
 import { Pokemon, Move } from "@/atoms/GameAtoms";
-interface Item {
-  name: string;
-  url: string;
-  formName?: string;
-  formInfo?: string;
-}
-const demonymValues: { [key: string]: string } = {
-  mega: "Mega",
-  galar: "Galarian",
-  alola: "Alolan",
-  hisui: "Hisuian",
-  paldea: "Paldean",
-};
 
 const otherExceptions = ["mr.", "jr."];
-export function pokemonCleanse(data: Item[]): Item[] {
-  const cleanedArray = data
-    .filter(({ name }) => name.includes("-"))
-    .map(({ name, url }) => {
-      const nameParts = name.split("-");
-      const pokemonName = nameParts[0];
-      const formParts = nameParts.slice(1);
-      const formName = formParts[0];
-      const formInfo = formParts.slice(1).join(" ");
-
-      const formDemonym =
-        demonymValues[formName.toLowerCase()] ||
-        formName.charAt(0).toUpperCase() + formName.slice(1);
-
-      return {
-        name: formDemonym + " " + pokemonName + " " + formInfo,
-        url,
-      };
-    });
-
-  const unprocessedArray = data.filter(({ name }) => !name.includes("-"));
-
-  return [
-    ...cleanedArray,
-    ...unprocessedArray.map(({ name, url }) => ({ name, url })),
-  ];
-}
-
-export function moveCleanse() {}
 
 export function updatePokemonData(pokemonData: Array<any>): Pokemon[] {
   return pokemonData.map((pokemon) => {
@@ -90,6 +48,14 @@ const exceptions = [
   "chien-pao",
   "chi-yu",
 ];
+const demonymValues: { [key: string]: string } = {
+  mega: "Mega",
+  galar: "Galarian",
+  alola: "Alolan",
+  hisui: "Hisuian",
+  paldea: "Paldean",
+};
+
 export function filterPokemonName(name: string) {
   if (!exceptions.includes(name)) {
     const parts = name.split("-");
