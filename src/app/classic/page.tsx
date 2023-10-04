9fd24170ad5d28efab4eeb541c077539bbaa158c
kadeem-lewis <email@kadeemlewis.com>
2023-10-04 02:01:24 -0400

    changed page to use prisma data instead of api data

--- src/app/classic/page.tsx
+++ src/app/classic/page.tsx
@@ -2,49 +2,17 @@ import { cache } from "react";
 import MyComboBox from "@/components/ui/MyComboBox";
 import OptionsBar from "@/components/layout/OptionsBar";
 import Gamebox from "@/components/core/Gamebox";
-import { updatePokemonData } from "@/helpers/DataProcessing";
 import { Pokemon } from "@/atoms/GameAtoms";
 import PokemonTypes from "@/components/core/PokemonTypes";
+import { prisma } from "@/lib/prisma";
 
-const query = `
-query MyQuery {
-  pokemon_v2_pokemon {
-    name
-    id
-    height
-    weight
-    pokemon_v2_pokemontypes {
-      pokemon_v2_type {
-        name
-      }
-    }
-    pokemon_v2_pokemonspecy {
-      generation_id
-    }
-  }
-}`;
 export const metadata = {
   title: "Pokedexle | Classic",
   description: "Guess Pokemon based on hints from previous guesses",
 };
-const getPokemonList = cache(async () => {
-  try {
-    const res = await fetch("https://beta.pokeapi.co/graphql/v1beta", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ query }),
-    });
-    const pokemonData = await res.json();
-    return updatePokemonData(pokemonData.data.pokemon_v2_pokemon);
-  } catch (err) {
-    console.error(err);
-  }
-});
 
 export default async function Classic() {
-  const pokemonList: Pokemon[] = (await getPokemonList()) || [];
+  const pokemonList: Pokemon[] = (await prisma.pokemon.findMany()) || [];
   return (
     <div>
       <OptionsBar />
