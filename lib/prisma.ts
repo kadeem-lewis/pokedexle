53d1ce53031e1fe3141b0f9aaa49333371dabf11
kadeem-lewis <email@kadeemlewis.com>
2023-10-05 05:24:45 -0400

    changed prisma lib setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+export default prisma;
