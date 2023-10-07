9952e7c3b742f3fde1a1bbdfefdab0645f3327c2
kadeem-lewis <email@kadeemlewis.com>
2023-10-07 06:18:38 -0400

    created route to add new daily entries

---
+++ app/api/daily/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(params: NextRequest) {
+  try {
+    const allPokemon = await prisma.pokemon.findMany();
+    const allMoves = await prisma.move.findMany();
+
+    // Fetch Moves and Pokémon that have been used in the Daily table
+    const usedClassicPokemonIds = await prisma.daily.findMany({
+      select: { classicId: true },
+    });
+    const usedWhosThatPokemonIds = await prisma.daily.findMany({
+      select: { whosThatPokemonId: true },
+    });
+    const usedMoveIds = await prisma.daily.findMany({
+      select: { moveId: true },
+    });
+
+    const unusedClassicPokemon = allPokemon.filter(
+      (pokemon) =>
+        !usedClassicPokemonIds.some((used) => used.classicId === pokemon.id)
+    );
+    const unusedWhosThatPokemon = allPokemon.filter(
+      (pokemon) =>
+        !usedWhosThatPokemonIds.some(
+          (used) => used.whosThatPokemonId === pokemon.id
+        )
+    );
+    const unusedMoves = allMoves.filter(
+      (move) => !usedMoveIds.some((used) => used.moveId === move.id)
+    );
+
+    // Random selection logic remains the same
+    const classic =
+      unusedClassicPokemon.length > 0
+        ? unusedClassicPokemon[
+            Math.floor(Math.random() * unusedClassicPokemon.length)
+          ]
+        : allPokemon[Math.floor(Math.random() * allPokemon.length)];
+
+    const whosThatPokemon =
+      unusedWhosThatPokemon.length > 0
+        ? unusedWhosThatPokemon[
+            Math.floor(Math.random() * unusedWhosThatPokemon.length)
+          ]
+        : allPokemon[Math.floor(Math.random() * allPokemon.length)];
+
+    // If there are no more unused Moves, throw an error
+    if (!unusedMoves.length) {
+      throw new Error("No unused Moves available.");
+    }
+
+    const move = unusedMoves[Math.floor(Math.random() * unusedMoves.length)];
+
+    // Set the date logic remains the same
+    const currentDate = new Date();
+    const nextDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate() + 1
+    );
+
+    const newDaily = await prisma.daily.create({
+      data: {
+        date: nextDate,
+        classicId: classic.id,
+        whosThatPokemonId: whosThatPokemon.id,
+        moveId: move.id,
+      },
+    });
+
+    console.log("New daily entry added:", newDaily);
+    return new Response("OK");
+  } catch (error) {
+    console.error("Error adding new daily entry:", error);
+    return new Response("Error");
+  }
+}
