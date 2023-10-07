8a101980a440dd5f05df338b47cfe1d459637300
kadeem-lewis <email@kadeemlewis.com>
2023-10-07 19:56:24 -0400

    created api route for getting latest dailies

---
+++ app/api/dailies/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { addDays, startOfDay } from "date-fns";
+
+export async function GET(request: Request) {
+  try {
+    const dailies = await prisma.daily.findMany({
+      orderBy: {
+        date: "desc",
+      },
+      take: 2,
+    });
+    return NextResponse.json(dailies);
+  } catch (error) {
+    return new Response("Error", { status: 500 });
+  }
+}
