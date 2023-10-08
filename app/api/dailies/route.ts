10758bd73cec24f91143b864fd705df4dfeb68e6
kadeem-lewis <email@kadeemlewis.com>
2023-10-07 22:50:57 -0400

    updated prisma imports

--- app/api/dailies/route.ts
+++ app/api/dailies/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { addDays, startOfDay } from "date-fns";
 
 export async function GET(request: Request) {
--- app/api/daily/route.ts
+++ app/api/daily/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { addDays, startOfDay } from "date-fns";
 
 export async function GET(params: NextRequest) {
