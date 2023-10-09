25148a0495388c5aaca5720b0928fc5c111fc061
kadeem-lewis <email@kadeemlewis.com>
2023-10-08 23:54:28 -0400

    added some content to modals to better explain site

--- components/content/GameOverContent.tsx
+++ components/content/GameOverContent.tsx
@@ -4,31 +4,42 @@ import {
   pokemonToGuessAtom,
   newGameAtom,
   currentGameMode,
+  guessAtom,
 } from "../../atoms/GameAtoms";
 import Image from "next/image";
 
 export default function GameOverContent() {
   const [mode, setMode] = useAtom(currentGameMode);
   const setNewGame = useSetAtom(newGameAtom);
+  const guesses = useAtomValue(guessAtom)[mode];
   const pokemonGuesses = useAtomValue(pokemonToGuessAtom);
   const correctAnswer = pokemonGuesses[mode];
   return (
     <div>
-      <h2>GameOverContent</h2>
-      <p>You won or Something idk</p>
+      <div className="text-3xl">
+        {guesses > 0 ? (
+          <p>You won!!</p>
+        ) : (
+          <p>You Lost. Better luck Next Time!</p>
+        )}
+      </div>
+
       {correctAnswer && (
-        <>
+        <div className="flex justify-center flex-col items-center">
           <Image
             src={correctAnswer.sprite}
             alt={`${correctAnswer.name} sprite`}
             width={200}
             height={200}
           />
-          <p>The answer was : {correctAnswer.name}</p>
-        </>
+          <p className="text-2xl">The answer was : {correctAnswer.name}</p>
+        </div>
       )}
       {mode === "classicUnlimited" ? (
-        <button onClick={() => setNewGame()}>New Game</button>
+        <div className="flex gap-4 justify-center text-2xl">
+          <span>Wanna try again?</span>
+          <button onClick={() => setNewGame()}>New Game</button>
+        </div>
       ) : (
         <p>New Game soon</p>
       )}
--- components/content/HowToPlayContent.tsx
+++ components/content/HowToPlayContent.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function HowToPlayContent() {
-  return <div>HowToPlay</div>;
+  return (
+    <div className=" text-2xl space-y-2">
+      <p>Guess the pokemon in 8 tries</p>
+      <hr />
+      <p>
+        Simply type in the name of a pokemon and it will reveal its properties
+      </p>
+      <hr />
+      <p>
+        The color of the tiles will change to show how close your guess was to
+        the pokemon to find.
+      </p>
+    </div>
+  );
 }
