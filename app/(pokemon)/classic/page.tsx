4242dcd13317b05c5e417b16378e475206e7b06a
kadeem-lewis <email@kadeemlewis.com>
2023-10-08 23:27:45 -0400

    reverted to using prisma query directly in server component

--- app/(pokemon)/classic/page.tsx
+++ app/(pokemon)/classic/page.tsx
@@ -1,19 +1,12 @@
 import Gamebox from "@/components/core/Gamebox";
+import { prisma } from "@/lib/prisma";
 
 export const metadata = {
   title: "Pokedexle | Classic",
   description: "Guess Pokemon based on hints from previous guesses",
 };
 
-async function getPokedex() {
-  const response = await fetch("/api/pokemon");
-
-  if (!response.ok) throw new Error("Failed to fetch data");
-
-  return response.json();
-}
-
 export default async function Classic() {
-  const pokedex = await getPokedex();
+  const pokedex = await prisma.pokemon.findMany();
   return <div>{pokedex && <Gamebox pokedex={pokedex} />}</div>;
 }
--- app/api/pokemon/route.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from "@/lib/prisma";
-
-export async function GET() {
-  const pokedex = await prisma.pokemon.findMany();
-  return Response.json(pokedex);
-}
