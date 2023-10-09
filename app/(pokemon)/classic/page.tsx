c882de325e7ed64105632960c0bc92de23697a16
kadeem-lewis <email@kadeemlewis.com>
2023-10-08 22:24:40 -0400

    removed direct prisma fetching from page and moved it to server routes

--- app/(pokemon)/classic/page.tsx
+++ app/(pokemon)/classic/page.tsx
@@ -1,22 +1,19 @@
 import Gamebox from "@/components/core/Gamebox";
-import { prisma } from "@/lib/prisma";
 
 export const metadata = {
   title: "Pokedexle | Classic",
   description: "Guess Pokemon based on hints from previous guesses",
 };
 
+async function getPokedex() {
+  const response = await fetch("/api/pokemon");
+
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  return response.json();
+}
+
 export default async function Classic() {
-  const pokedex = await prisma.pokemon.findMany();
-  const dailies = await prisma.daily.findMany({
-    orderBy: {
-      date: "desc",
-    },
-    take: 2,
-  });
-  return (
-    <div>
-      {pokedex && dailies && <Gamebox pokedex={pokedex} dailies={dailies} />}
-    </div>
-  );
+  const pokedex = await getPokedex();
+  return <div>{pokedex && <Gamebox pokedex={pokedex} />}</div>;
 }
