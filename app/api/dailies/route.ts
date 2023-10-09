f82f87657b71b854dc670c90f7b8adefd25bd211
kadeem-lewis <email@kadeemlewis.com>
2023-10-08 22:24:07 -0400

    updated fetching daily function to properly convert dates and provide error handling

--- app/api/dailies/route.ts
+++ app/api/dailies/route.ts
@@ -1,14 +1,20 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { addDays, startOfDay } from "date-fns";
+import { startOfDay, subMinutes } from "date-fns";
 
 export async function GET(request: Request) {
   try {
-    const dailies = await prisma.daily.findMany({
-      orderBy: {
-        date: "desc",
+    const url = new URL(request.url);
+    const dateString = url.searchParams.get("date");
+
+    if (dateString === null) throw new Error("dateString is null");
+    const date = startOfDay(new Date(dateString));
+    const utcDate = subMinutes(date, date.getTimezoneOffset());
+
+    const dailies = await prisma.daily.findUnique({
+      where: {
+        date: utcDate,
       },
-      take: 2,
     });
     return NextResponse.json(dailies);
   } catch (error) {
