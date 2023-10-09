cceb90d25311a5e07ca80b5dc448fdb99559a3a8
kadeem-lewis <email@kadeemlewis.com>
2023-10-08 22:34:50 -0400

    removed request object that wasnt being used

--- app/api/pokemon/route.ts
+++ app/api/pokemon/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 
-export async function GET(request: Request) {
+export async function GET() {
   const pokedex = await prisma.pokemon.findMany();
   return Response.json(pokedex);
 }
