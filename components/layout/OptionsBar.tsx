41304fe6278fb1cebb450cbbc85abd64b6421024
kadeem-lewis <email@kadeemlewis.com>
2023-10-10 16:43:27 -0400

    created a calendar component and added it to the options bars

--- components/layout/OptionsBar.tsx
+++ components/layout/OptionsBar.tsx
@@ -6,14 +6,16 @@ import SettingsContent from "../content/SettingsContent";
 import AboutContent from "../content/AboutContent";
 import StatsContent from "../content/StatsContent";
 import HowToPlayContent from "../content/HowToPlayContent";
+import Calendar from "../content/Calendar";
 
 export default function OptionsBar() {
   const [settingClick, setSettingClick] = useState(false);
   const [aboutClick, setAboutClick] = useState(false);
   const [statsClick, setStatsClick] = useState(false);
   const [howToPlayClick, setHowToPlayClick] = useState(false);
+  const [calendarClick, setCalendarClick] = useState(false);
   return (
-    <nav className=" grid grid-cols-4 border-2 border-current">
+    <nav className=" grid grid-cols-5 border-2 border-current">
       <button
         onClick={() => setHowToPlayClick(true)}
         className="border-r-2 border-current py-1"
@@ -43,6 +45,22 @@ export default function OptionsBar() {
           title="Settings"
         >
           <SettingsContent />
+        </OptionsModal>
+      )}
+
+      <button
+        onClick={() => setCalendarClick(true)}
+        className="border-r-2 border-current py-1"
+      >
+        <Icons.calendar className="mx-auto h-6 w-6 text-current" />
+      </button>
+      {calendarClick && (
+        <OptionsModal
+          isOpen={calendarClick}
+          setIsOpen={setCalendarClick}
+          title="Calendar"
+        >
+          <Calendar />
         </OptionsModal>
       )}
 
