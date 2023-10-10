a777553eaf789d41df31712bf2527ff78418b4f3
kadeem-lewis <email@kadeemlewis.com>
2023-10-10 01:50:15 -0400

    created a component to show the countdown until the next daily game

---
+++ components/Countdown.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { differenceInSeconds } from "date-fns";
+
+export default function Countdown({ targetDate }: { targetDate: Date }) {
+  const [timeLeft, setTimeLeft] = useState<number>(
+    calculateTimeLeft(targetDate)
+  );
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newTimeLeft = calculateTimeLeft(targetDate);
+
+      if (newTimeLeft <= 0) {
+        clearInterval(intervalId);
+      }
+
+      setTimeLeft(newTimeLeft);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [targetDate]);
+
+  function calculateTimeLeft(endDate: Date): number {
+    const now = new Date();
+    return differenceInSeconds(endDate, now);
+  }
+
+  function formatTimeLeft(timeLeftInSeconds: number): string {
+    const hours = Math.floor(timeLeftInSeconds / 3600);
+    let remaining = timeLeftInSeconds % 3600;
+
+    const minutes = Math.floor(remaining / 60);
+    remaining = remaining % 60;
+
+    const seconds = remaining;
+
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  }
+
+  return (
+    <div>
+      {timeLeft <= 0 ? (
+        <p>Time&apos;s up!</p>
+      ) : (
+        <span className="text-3xl">{formatTimeLeft(timeLeft)}</span>
+      )}
+    </div>
+  );
+}
