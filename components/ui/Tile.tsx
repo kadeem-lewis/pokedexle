ebebd6ab51e87926e3819ffee88384ed4f3a1cab
kadeem-lewis <email@kadeemlewis.com>
2023-10-10 04:38:58 -0400

    created a reuseable component that will act as container for individual grid tiles in guesses

---
+++ components/ui/Tile.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+//proposed reusable component that would be responsible for showing the titles in the FeedbackRow?
+export default function Tile() {
+  return <div>Tile</div>;
+}
