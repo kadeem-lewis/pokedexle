87217189c3cf0fce28900758ade251fbe6bcd3be
kadeem-lewis <email@kadeemlewis.com>
2023-10-11 13:16:01 -0400

    reduced the opacity of the white background on card

--- components/ui/LinkCard.tsx
+++ components/ui/LinkCard.tsx
@@ -17,7 +17,7 @@ export default function LinkCard({
       className="my-6  rounded-lg text-white border-amber-300 border-4 outline-3 outline outline-black dark:outline-white dark:border-amber-500"
       style={{ backgroundImage: `url(${background})` }}
     >
-      <div className="w-full h-full bg-white/20 p-4">
+      <div className="w-full h-full bg-white/10 p-4">
         <Link href={path}>
           <p className="text-3xl drop-shadow-md uppercase text-shadow">
             {title}
