8a2eeaf552716d9c8c861d61bd6939a59693fca2
kadeem-lewis <email@kadeemlewis.com>
2023-10-10 23:28:02 -0400

    added white background over image to make text stand out more

--- components/ui/LinkCard.tsx
+++ components/ui/LinkCard.tsx
@@ -14,13 +14,17 @@ export default function LinkCard({
 }: Props) {
   return (
     <div
-      className="my-6 p-4 rounded-lg text-white border-amber-300 border-4 outline-3 outline outline-black dark:outline-white dark:border-amber"
+      className="my-6  rounded-lg text-white border-amber-300 border-4 outline-3 outline outline-black dark:outline-white dark:border-amber-500"
       style={{ backgroundImage: `url(${background})` }}
     >
-      <Link href={path}>
-        <p className="text-3xl drop-shadow-md uppercase text-shadow">{title}</p>
-        <p className="text-2xl text-shadow">{description}</p>
-      </Link>
+      <div className="w-full h-full bg-white/20 p-4">
+        <Link href={path}>
+          <p className="text-3xl drop-shadow-md uppercase text-shadow">
+            {title}
+          </p>
+          <p className="text-2xl text-shadow">{description}</p>
+        </Link>
+      </div>
     </div>
   );
 }
