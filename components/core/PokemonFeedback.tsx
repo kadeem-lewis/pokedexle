c17ed241883ae421f86e3d0351ec5eb659fd306c
kadeem-lewis <email@kadeemlewis.com>
2023-10-12 04:18:33 -0400

    removed unused variable

--- components/core/PokemonFeedback.tsx
+++ components/core/PokemonFeedback.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React, { useEffect, useMemo, useState } from "react";
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import {
-  classicGameOver,
   currentGameMode,
   gameOverAtom,
   guessAtom,
