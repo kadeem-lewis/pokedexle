710f332456a0208c2d27f5bb8917a9ff9d7c20bf
kadeem-lewis <email@kadeemlewis.com>
2023-10-12 23:00:19 -0400

    added sample content to the about page

--- components/content/AboutContent.tsx
+++ components/content/AboutContent.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function AboutContent() {
-  return <div>AboutContent</div>;
+  return (
+    <div className="text-2xl">
+      <p>
+        Pokedexle is a variety of different games themed around Guessing
+        Pokedexle. It was heavily inspired by games such as Wordle, Squirdle,
+        Pokedexle.app and Loldle
+      </p>
+      <hr />
+      <p>
+        Created by Kadeem Lewis. Special thanks to PokeApi for supplying all the
+        pokemon data and sprites
+      </p>
+    </div>
+  );
 }
