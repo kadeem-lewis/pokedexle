8b74ad8cee82a1ee3c5bbd57da6e8f60fcdba80b
kadeem-lewis <email@kadeemlewis.com>
2023-10-16 22:43:21 -0400

    began setting up process of adding tooltip of guesses icons

--- components/core/Guesses.tsx
+++ components/core/Guesses.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Icons } from "../Icons";
 import { useAtomValue } from "jotai";
-import { currentGameMode, guessAtom } from "@/atoms/GameAtoms";
+import { Tooltip } from "react-tooltip";
+import {
+  currentGameMode,
+  guessAtom,
+  guessedItemsAtom,
+} from "@/atoms/GameAtoms";
 import { defaultGuesses } from "@/constants";
 
 export default function Guesses() {
   const mode = useAtomValue(currentGameMode);
   const guesses = useAtomValue(guessAtom)[mode];
+  const guessedItems = useAtomValue(guessedItemsAtom)[mode];
 
   return (
     <div className="my-4 flex flex-row justify-end gap-0.5 ">
       {[...Array(defaultGuesses)].map((value, index) => (
         <Icons.pokeball
           key={index}
+          data-tooltip-id="guess-data"
           className={`h-6 w-6 ${index + 1 > guesses ? "grayscale" : ""}`}
         />
       ))}
