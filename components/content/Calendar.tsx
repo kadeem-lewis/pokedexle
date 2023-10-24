88c0d2b632f31cbe1e7391f895e31ddb46731f23
kadeem-lewis <email@kadeemlewis.com>
2023-10-24 01:49:11 -0400

    commented out calendar component until it will be worked on

--- components/content/Calendar.tsx
+++ components/content/Calendar.tsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import { DayPicker } from "react-day-picker";
-import { useAtom } from "jotai";
-import { dateAtom } from "@/atoms/GameAtoms";
-import { addDays, endOfMonth, format } from "date-fns";
-// Your App.tsx file
-import 'react-day-picker/dist/style.css';
+// import React, { useState } from "react";
+// import { DayPicker } from "react-day-picker";
+// import { useAtom } from "jotai";
+// import { dateAtom } from "@/atoms/GameAtoms";
+// import { addDays, endOfMonth, format } from "date-fns";
+// // Your App.tsx file
+// import 'react-day-picker/dist/style.css';
 
-export default function Calendar() {
-  const [selectedDate, setSelectedDate] = useAtom(dateAtom)
-  const handleDayClick = (date:Date)=> {
-    console.log(date)
-    setSelectedDate(date)
-    console.log(selectedDate)
-  };
-  //disable all days before id 1 in the daily table
-  const disabledDays = [{from: addDays(new Date(), 1),to: endOfMonth(new Date())}]
-  const footer = selectedDate ? (
-    <p>You selected {format(selectedDate, 'PPP')}.</p>
-  ) : (
-    <p>Please pick a day.</p>
-  );
+// export default function Calendar() {
+//   const [selectedDate, setSelectedDate] = useAtom(dateAtom)
+//   const handleDayClick = (date:Date)=> {
+//     console.log(date)
+//     setSelectedDate(date)
+//     console.log(selectedDate)
+//   };
+//   //disable all days before id 1 in the daily table
+//   const disabledDays = [{from: addDays(new Date(), 1),to: endOfMonth(new Date())}]
+//   const footer = selectedDate ? (
+//     <p>You selected {format(selectedDate, 'PPP')}.</p>
+//   ) : (
+//     <p>Please pick a day.</p>
+//   );
 
-  return (
-  <div className="flex justify-start items-center w-full text-2xl">
-    <DayPicker mode="single" selected={selectedDate} onDayClick={handleDayClick} toDate={new Date()} disabled={disabledDays} footer={footer}/>
-  </div>
-    )
-}
+//   return (
+//   <div className="flex justify-start items-center w-full text-2xl">
+//     <DayPicker mode="single" selected={selectedDate} onDayClick={handleDayClick} toDate={new Date()} disabled={disabledDays} footer={footer}/>
+//   </div>
+//     )
+// }
