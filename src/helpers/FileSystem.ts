3f90edec3d2e7686500d280049871e39617bdb03
kadeem-lewis <email@kadeemlewis.com>
2023-10-27 14:51:14 -0400

    changed fileSystem function to include src folder

--- src/helpers/FileSystem.ts
+++ src/helpers/FileSystem.ts
@@ -1,9 +1,9 @@
-import type { Move, Pokemon } from "@/app/atoms/GameAtoms";
+import type { Move, Pokemon } from "@/atoms/GameAtoms";
 import { promises as fs } from "fs";
 
 export async function readJson(path: string): Promise<Pokemon[] | Move[]> {
   try {
-    const fullPath = process.cwd() + path;
+    const fullPath = process.cwd() + "/src" + path;
     const data = await fs.readFile(fullPath, "utf8");
     return JSON.parse(data);
   } catch (error) {
