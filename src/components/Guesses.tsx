bfdcc4660871f1b4d99f6db556a6c9729a9ff7c4
kadeem-lewis <email@kadeemlewis.com>
2023-10-31 21:31:13 -0400

    removed unneeded import

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import { Icons } from "./Icons";
 import { useAtomValue } from "jotai";
 import { Tooltip } from "react-tooltip";
