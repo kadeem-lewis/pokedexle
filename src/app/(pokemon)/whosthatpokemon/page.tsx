50185c587cfd08fd8e3af6d95e5f2cabb8a9a17b
kadeem-lewis <email@kadeemlewis.com>
2023-11-19 07:47:56 -0500

    format date into string

--- src/app/(pokemon)/whosthatpokemon/page.tsx
+++ src/app/(pokemon)/whosthatpokemon/page.tsx
@@ -4,7 +4,7 @@ import Guesses from "@/components/Guesses";
 import ModeSwitch from "@/components/ModeSwitch";
 import { readJson } from "@/helpers/FileSystem";
 import { prisma } from "@/lib/prisma";
-import { startOfToday } from "date-fns";
+import { startOfToday, format } from "date-fns";
 
 type WhosThatPokemonProps = {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -21,7 +21,7 @@ export default async function WhosThatPokemon({
   const pokedex = (await readJson("/data/pokedex.json")) as Pokemon[];
   const dailies = await prisma.daily.findUnique({
     where: {
-      date: startOfToday(),
+      date: format(new Date(), "yyyy-MM-dd")
     },
   });
   return (
