e9faa020b0ade568e8753bd39a0543ed9236dfe1
kadeem-lewis <email@kadeemlewis.com>
2024-02-03 19:32:51 -0500

    updated the readJson function to use path.join() to solve file not being found in deployment

--- src/helpers/FileSystem.ts
+++ src/helpers/FileSystem.ts
@@ -1,9 +1,11 @@
 import type { Pokemon } from "@/atoms/GameAtoms";
 import { promises as fs } from "fs";
+import path from "path";
 
-export async function readJson(path: string): Promise<Pokemon[]> {
+export async function readJson(filePath: string): Promise<Pokemon[]> {
   try {
-    const fullPath = process.cwd() + "/src" + path;
+    const segments = filePath.split("/");
+    const fullPath = path.join(process.cwd(), "src", ...segments);
     const data = await fs.readFile(fullPath, "utf8");
     return JSON.parse(data);
   } catch (error) {
