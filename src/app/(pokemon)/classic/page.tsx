cdfaf732e0616f746c291b2e5a7be4887d767cad
kadeem-lewis <email@kadeemlewis.com>
2024-02-17 22:09:34 -0500

    updated components to no longer need readJson function and instead directly import json file

--- src/app/(pokemon)/classic/page.tsx
+++ src/app/(pokemon)/classic/page.tsx
@@ -1,7 +1,6 @@
 import GameWrapper from "@/components/GameWrapper";
 import Guesses from "@/components/Guesses";
 import ModeSwitch from "@/components/ModeSwitch";
-import { readJson } from "@/helpers/FileSystem";
 import pokedex from "@/data/pokedex.json";
 
 export const metadata = {
