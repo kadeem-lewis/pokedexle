c6a18cb17e8fdfcb205e199b0339e64dae24bf25
kadeem-lewis <email@kadeemlewis.com>
2024-02-24 05:12:56 -0500

    updated footer to use a dynamic year

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <div className="text-center">
-    <p className=" text-2xl">© 2023 All Rights Reserved</p>
-    <p className="text-xl">Nintendo does not endorse or sponsor this project.</p>
+      <p className=" text-2xl">{year} - pokedexle.com</p>
+      <p className="text-xl">
+        Nintendo does not endorse or sponsor this project.
+      </p>
     </div>
   );
 }
