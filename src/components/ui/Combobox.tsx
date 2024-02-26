c1d0ee2ede7805ad5f32818647334428e452eadd
kadeem-lewis <email@kadeemlewis.com>
2024-02-26 01:43:50 -0500

    set size of popover to be inherited from parent

--- src/components/ui/Combobox.tsx
+++ src/components/ui/Combobox.tsx
@@ -27,7 +27,7 @@ export function ComboBox<T extends object>({
         <Input className="w-full border-b-2 border-dashed border-foreground bg-transparent py-1 pl-3" />
         <Button className="hidden">▼</Button>
       </Group>
-      <Popover className="entering:animate-in entering:fade-in exiting:animate-out exiting:fade-out max-h-60 w-[--trigger-width] overflow-auto rounded-md bg-primary text-base shadow-lg ring-1 ring-black/5">
+      <Popover className="entering:animate-in entering:fade-in exiting:animate-out exiting:fade-out max-h-60 w-[inherit] overflow-auto rounded-md bg-primary text-base shadow-lg ring-1 ring-black/5">
         <ListBox className="max-h-[inherit] overflow-auto p-1 outline-0 [clip-path:inset(0_0_0_0_round_.75rem)]">
           {children}
         </ListBox>
