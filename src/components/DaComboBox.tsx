6e53c8f7518e1813b5cb37c4aae36daa73ca0478
kadeem-lewis <email@kadeemlewis.com>
2024-02-25 22:19:56 -0500

    updated react aria combobox see that selection works

--- src/components/DaComboBox.tsx
+++ src/components/DaComboBox.tsx
@@ -1,11 +1,10 @@
 "use client";
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { ComboBox, ComboBoxItem } from "./ui/Combobox";
 import PokemonCard from "./ui/PokemonCard";
 import Fuse from "fuse.js";
 import {
   addGuessedItemAtom,
-  Pokemon,
   pokedexAtom,
   currentGameMode,
   guessedItemsAtom,
@@ -15,8 +14,13 @@ import { useSetAtom, useAtomValue } from "jotai";
 import { Button } from "./ui/Button";
 
 export default function DaComboBox() {
-  const [selected, setSelected] = useState<Pokemon | null>(null);
-  const [query, setQuery] = useState("");
+  const [fieldState, setFieldState] = useState<{
+    selectedKey: Key | null;
+    inputValue: string;
+  }>({
+    selectedKey: null,
+    inputValue: "",
+  });
   const [error, setError] = useState(false);
   const pokedex = useAtomValue(pokedexAtom);
   const mode = useAtomValue(currentGameMode);
@@ -33,48 +37,60 @@ export default function DaComboBox() {
   );
 
   const filteredItems =
-    query === ""
+    fieldState.inputValue === ""
       ? pokedex.slice(0, 6)
       : fuse
-          .search(query)
+          .search(fieldState.inputValue)
           .map((res) => ({ ...res.item }))
           .slice(0, 6);
 
   const onInputChange = (value: string) => {
-    setQuery(value);
+    setFieldState((prevState) => ({
+      inputValue: value,
+      selectedKey: value === "" ? null : prevState.selectedKey,
+    }));
   };
 
-  const onSelectionChange = (pokemon: Key) => {
-    setSelected(pokedex.find((p) => p.name === pokemon) ?? null);
+  const onSelectionChange = (id: Key) => {
+    setFieldState({
+      inputValue: pokedex.find((p) => p.id === id)?.name ?? "",
+      selectedKey: id,
+    });
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (selected && selected.name) {
-      if (guessedItems.some((item) => item.name === selected.name)) {
+    if (fieldState.selectedKey && fieldState.inputValue) {
+      if (guessedItems.some((item) => item.name === fieldState.inputValue)) {
         setError(true);
       } else {
-        addNewGuess(selected);
-        setError(false);
+        const pokemon = pokedex.find((p) => p.name === fieldState.inputValue);
+        if (pokemon) {
+          addNewGuess(pokemon);
+          setError(false);
+        }
       }
-      setSelected(null);
-      setQuery("");
+      setFieldState({ selectedKey: null, inputValue: "" });
     }
   };
 
   return (
     <>
-      {selected}
       <form onSubmit={handleSubmit} className="my-4 flex flex-row gap-2">
         <ComboBox
           defaultItems={filteredItems}
-          inputValue={selected?.name}
+          selectedKey={fieldState.selectedKey}
+          inputValue={fieldState.inputValue}
           onInputChange={onInputChange}
           onSelectionChange={onSelectionChange}
           aria-label="Search pokémon by name or type..."
+          className="flex w-full flex-col gap-2"
         >
           {(item) => (
-            <ComboBoxItem textValue={item.name}>
+            <ComboBoxItem
+              textValue={item.name}
+              className="relative cursor-default select-none border-b border-foreground capitalize"
+            >
               <PokemonCard pokemon={item} />
             </ComboBoxItem>
           )}
@@ -82,7 +98,7 @@ export default function DaComboBox() {
         <Button
           type="submit"
           variant="flat"
-          className="flex-none cursor-pointer bg-purple-300   hover:bg-purple-400"
+          className="flex-none cursor-pointer bg-purple-300 hover:bg-purple-400"
         >
           Submit
         </Button>
