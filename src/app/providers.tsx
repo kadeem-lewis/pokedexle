9540e66f923f5e2418eb5eaa40722e6782cc56fd
kadeem-lewis <email@kadeemlewis.com>
2024-04-15 17:36:53 -0400

    wrapped app in posthog provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,18 +3,28 @@ import React from "react";
 import { ThemeProvider } from "next-themes";
 import { Provider as JotaiProvider } from "jotai";
 import { DevTools } from "jotai-devtools";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 type ProviderProps = {
   children: React.ReactNode;
 };
 
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  });
+}
+
 export default function Providers({ children }: ProviderProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <JotaiProvider>
-        <DevTools />
-        {children}
-      </JotaiProvider>
-    </ThemeProvider>
+    <PostHogProvider client={posthog}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <JotaiProvider>
+          <DevTools />
+          {children}
+        </JotaiProvider>
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }
