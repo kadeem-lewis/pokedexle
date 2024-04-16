58aaec2f7cfd593d4293aecd8f1dee30dc531761
kadeem-lewis <email@kadeemlewis.com>
2024-04-16 02:47:36 -0400

    generated types for icon names

---
+++ src/types/Icons.d.ts
@@ -0,0 +1,14 @@
+export type IconName =
+  | "pixelarticons-sun"
+  | "pixelarticons-moon-star"
+  | "pixelarticons-check"
+  | "pixelarticons-calendar-month"
+  | "heroicons-cog-6-tooth-solid"
+  | "heroicons-question-mark-circle-solid"
+  | "heroicons-chart-bar-solid"
+  | "heroicons-exclamation-circle-solid"
+  | "mdi-pokeball"
+  | "pixelarticons-chevron-right"
+  | "pixelarticons-chevron-up"
+  | "heroicons-share-solid"
+  | "pixelarticons-close";
