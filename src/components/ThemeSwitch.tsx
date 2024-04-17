43ac172affdd40bb1ad9c5fcb951f171d3ceea79
kadeem-lewis <email@kadeemlewis.com>
2024-04-17 05:37:35 -0400

    updated theme switch to use new pokemon icons

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -20,19 +20,16 @@ export default function ThemeSwitch() {
           name="theme-toggle"
           aria-label="Toggle Dark Mode"
           onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
-          className="transform rounded-md border-2 border-foreground p-1 transition duration-200 ease-in-out hover:bg-foreground hover:text-background"
+          className="inline-flex transform items-center justify-center rounded-md border-2 border-foreground p-1 text-fg transition duration-200 ease-in-out hover:bg-bg-muted"
         >
-          {resolvedTheme === "light" ? (
-            <Icon
-              name="pixelarticons-moon-star"
-              className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-            />
-          ) : (
-            <Icon
-              name="pixelarticons-sun"
-              className="size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-            />
-          )}
+          <Icon
+            name="pokemon-moon"
+            className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Icon
+            name="pokemon-sun"
+            className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
         </button>
       )}
     </div>
