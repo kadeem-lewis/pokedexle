9eaa344437a473c7f399fabf0ef41af5539215c1
kadeem-lewis <email@kadeemlewis.com>
2024-04-17 05:37:08 -0400

    fixed error in Icon tag styles

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,7 +4,7 @@ type IconProps = React.HTMLAttributes<SVGElement> & { name: IconName };
 
 export function Icon({ name, className, ...props }: IconProps) {
   return (
-    <svg className={`"inline self-center" ${className}`} {...props}>
+    <svg className={`inline self-center ${className}`} {...props}>
       <use href={`./icons/spritesheet.svg#${name}`} />
     </svg>
   );
