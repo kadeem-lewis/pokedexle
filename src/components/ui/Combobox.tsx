2113daf7002d0e9632e9030d030afabbed123b50
kadeem-lewis <email@kadeemlewis.com>
2024-04-17 05:45:10 -0400

    updated combobox to use new color scheme

--- src/components/ui/Combobox.tsx
+++ src/components/ui/Combobox.tsx
@@ -27,7 +27,7 @@ export function ComboBox<T extends object>({
         <Input className="w-full border-b-2 border-dashed border-foreground bg-transparent py-1 pl-3" />
         <Button className="hidden">▼</Button>
       </Group>
-      <Popover className="entering:animate-in entering:fade-in exiting:animate-out exiting:fade-out max-h-60 w-[inherit] overflow-auto rounded-md bg-primary text-base shadow-lg ring-1 ring-black/5">
+      <Popover className="entering:animate-in entering:fade-in exiting:animate-out exiting:fade-out max-h-60 w-[inherit] overflow-auto rounded-md bg-bg-panel text-base shadow-lg ring-1 ring-black/5">
         <ListBox className="max-h-[inherit] overflow-auto p-1 outline-0 [clip-path:inset(0_0_0_0_round_.75rem)]">
           {children}
         </ListBox>
@@ -41,7 +41,7 @@ export function ComboBoxItem(props: ListBoxItemProps) {
     <ListBoxItem
       {...props}
       className={({ isFocused, isSelected }) =>
-        `my-item ${isFocused ? "bg-teal-600" : ""} ${isSelected ? "bg-teal-600" : ""} border-b border-foreground capitalize`
+        `my-item ${isFocused ? "bg-teal-600" : ""} ${isSelected ? "bg-teal-600" : ""} border-border border-b capitalize`
       }
     />
   );
