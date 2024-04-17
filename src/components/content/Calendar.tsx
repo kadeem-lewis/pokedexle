f773c53b1b816885461d5737d97716f39c300e16
kadeem-lewis <email@kadeemlewis.com>
2024-04-17 05:43:12 -0400

    began using new calendar component

--- src/components/content/Calendar.tsx
+++ src/components/content/Calendar.tsx
@@ -1,28 +1,6 @@
 "use client";
-//import React, { useState } from "react";
-//import { useAtom } from "jotai";
-//import { addDays, endOfMonth, format } from "date-fns";
-// Your App.tsx file
+import { Calendar as MyCalendar } from "../ui/Calendar";
 
 export default function Calendar() {
-  // const [selectedDate, setSelectedDate] = useAtom(dateAtom)
-  // const handleDayClick = (date:Date)=> {
-  //   console.log(date)
-  //   setSelectedDate(date)
-  //   console.log(selectedDate)
-  // };
-  // //disable all days before id 1 in the daily table
-  // const disabledDays = [{from: addDays(new Date(), 1),to: endOfMonth(new Date())}]
-  // const footer = selectedDate ? (
-  //   <p>You selected {format(selectedDate, 'PPP')}.</p>
-  // ) : (
-  //   <p>Please pick a day.</p>
-  // );
-
-  return (
-    <div className="flex w-full items-center justify-start text-2xl">
-      Coming Soon
-      {/* <DayPicker mode="single" selected={selectedDate} onDayClick={handleDayClick} toDate={new Date()} disabled={disabledDays} footer={footer}/> */}
-    </div>
-  );
+  return <MyCalendar />;
 }
