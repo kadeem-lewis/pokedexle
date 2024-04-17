661598998be017b91aa33f84a08080f74b7d8560
kadeem-lewis <email@kadeemlewis.com>
2024-04-17 06:11:52 -0400

    began testing getting date from calendar for archives

--- src/components/content/Calendar.tsx
+++ src/components/content/Calendar.tsx
@@ -1,6 +1,17 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Calendar as MyCalendar } from "../ui/Calendar";
+import { getLocalTimeZone, today } from "@internationalized/date";
+import { useRouter } from "next/navigation";
 
 export default function Calendar() {
-  return <MyCalendar />;
+  const [date, setDate] = useState(today(getLocalTimeZone()));
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("?date=" + date.toString());
+    router.refresh();
+  }, [date, router]);
+
+  return <MyCalendar date={date} onChange={setDate} />;
 }
