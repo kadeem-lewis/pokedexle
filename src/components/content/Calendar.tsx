2603286251d508d331bfa2c81f8de74cb8df8418
kadeem-lewis <email@kadeemlewis.com>
2024-04-21 07:01:36 -0400

    updated calendar to update url and date atom on clicking a new date

--- src/components/content/Calendar.tsx
+++ src/components/content/Calendar.tsx
@@ -1,18 +1,36 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Calendar as MyCalendar } from "../ui/Calendar";
-import { getLocalTimeZone, today } from "@internationalized/date";
-import { useRouter } from "next/navigation";
+import { getLocalTimeZone, today, isEqualDay } from "@internationalized/date";
+import { usePathname, useRouter } from "next/navigation";
 import { DateValue } from "react-aria-components";
+import { useAtom } from "jotai";
+import { dateAtom } from "@/atoms/GameAtoms";
 
 export default function Calendar() {
   const [date, setDate] = useState<DateValue>(today(getLocalTimeZone()));
+  const [, setAtomDate] = useAtom(dateAtom);
+
   const router = useRouter();
+  const pathname = usePathname();
 
-  useEffect(() => {
-    router.push("?date=" + date.toString());
-    router.refresh();
-  }, [date, router]);
+  const handleDateChange = (date: DateValue) => {
+    setDate(date);
+    setAtomDate(date.toString());
 
-  return <MyCalendar value={date} onChange={setDate} />;
+    if (!isEqualDay(date, today(getLocalTimeZone()))) {
+      router.push("?date=" + date.toString());
+    } else {
+      router.push(pathname);
+    }
+  };
+
+  return (
+    <MyCalendar
+      aria-label="Puzzle Date"
+      value={date}
+      onChange={handleDateChange}
+      maxValue={today(getLocalTimeZone())}
+    />
+  );
 }
