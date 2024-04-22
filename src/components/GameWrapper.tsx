99b0a6313fd55d134f48237800f634a77ef57cc4
kadeem-lewis <email@kadeemlewis.com>
2024-04-22 04:46:15 -0400

    game wrapper now handles setting pokemon to guess

--- src/components/GameWrapper.tsx
+++ src/components/GameWrapper.tsx
@@ -1,12 +1,14 @@
 "use client";
 import {
   Pokemon,
+  classicPracticeSolutionAtom,
   currentGameMode,
   dailyDataAtom,
-  dailyPokemonAtom,
   dateAtom,
   gameOverAtom,
   pokedexAtom,
+  pokemonToGuessAtom,
+  whosthatpokemonPracticeSolutionAtom,
 } from "@/atoms/GameAtoms";
 import { useAtomValue, useAtom, useSetAtom } from "jotai";
 import { useHydrateAtoms } from "jotai/utils";
@@ -25,11 +27,15 @@ type GameWrapperProps = {
 export default function GameWrapper({ pokedex }: GameWrapperProps) {
   useHydrateAtoms([[pokedexAtom, pokedex]]);
   const [mode, setMode] = useAtom(currentGameMode);
-  const setDailyPokemon = useSetAtom(dailyPokemonAtom);
   const gameOver = useAtomValue(gameOverAtom);
   const [{ data, isPending, isError }] = useAtom(dailyDataAtom);
   const [, setAtomDate] = useAtom(dateAtom);
+  const setPokemonToGuess = useSetAtom(pokemonToGuessAtom);
   console.log(data, isPending, isError);
+  const classicPracticeSolution = useAtomValue(classicPracticeSolutionAtom);
+  const whosthatpokemonPracticeSolution = useAtomValue(
+    whosthatpokemonPracticeSolutionAtom,
+  );
 
   const currentPath = usePathname();
   const searchParams = useSearchParams();
@@ -54,21 +60,66 @@ export default function GameWrapper({ pokedex }: GameWrapperProps) {
   }, [searchParams, setAtomDate]);
 
   useEffect(() => {
-    console.log("dailyData", data);
-    function setDailies() {
-      const dailyClassicPokemon =
-        pokedex.find((pokemon) => pokemon.id === data?.classicId) ?? null;
-      const dailyWhosThatPokemon =
-        pokedex.find((pokemon) => pokemon.id === data?.whosThatPokemonId) ??
-        null;
-      setDailyPokemon((prev) => ({
-        ...prev,
-        whosthatpokemon: dailyWhosThatPokemon,
-        classic: dailyClassicPokemon,
-      }));
+    // Setting daily values from fetched data
+    const dailyClassicPokemon =
+      pokedex.find((pokemon) => pokemon.id === data?.classicId) ?? null;
+    const dailyWhosThatPokemon =
+      pokedex.find((pokemon) => pokemon.id === data?.whosThatPokemonId) ?? null;
+
+    // Setting up the initial states
+    const initialStates = {
+      classic: dailyClassicPokemon,
+      whosthatpokemon: dailyWhosThatPokemon,
+      classicUnlimited: classicPracticeSolution,
+      whosthatpokemonUnlimited: whosthatpokemonPracticeSolution,
+    };
+
+    // Getting parameters from search
+    const searchParamAnswer = searchParams.has("x")
+      ? decodeURIComponent(atob(String(searchParams.get("x"))))
+      : null;
+    let answer;
+    if (searchParamAnswer && !isNaN(Number(searchParamAnswer))) {
+      answer = pokedex.find(
+        (pokemon) => pokemon.id === Number(searchParamAnswer),
+      );
     }
-    setDailies();
-  }, [data, pokedex, setDailyPokemon]);
+
+    // Apply logic based on game mode and available data
+    if (answer) {
+      if (mode === "classicUnlimited") {
+        initialStates.classicUnlimited = answer;
+      } else if (mode === "whosthatpokemonUnlimited") {
+        initialStates.whosthatpokemonUnlimited = answer;
+      }
+    } else {
+      // If no answer from URL or local storage, assign random pokemon
+      const randomPokemon = pokedex[Math.floor(Math.random() * pokedex.length)];
+      if (mode === "classicUnlimited" && !initialStates.classicUnlimited) {
+        initialStates.classicUnlimited = randomPokemon;
+      }
+      if (
+        mode === "whosthatpokemonUnlimited" &&
+        !initialStates.whosthatpokemonUnlimited
+      ) {
+        initialStates.whosthatpokemonUnlimited = randomPokemon;
+      }
+    }
+
+    // Update state atom with these initialized values
+    setPokemonToGuess((prev) => ({
+      ...prev,
+      ...initialStates,
+    }));
+  }, [
+    data,
+    pokedex,
+    searchParams,
+    mode,
+    setPokemonToGuess,
+    classicPracticeSolution,
+    whosthatpokemonPracticeSolution,
+  ]);
 
   return (
     <>
