09e02f6ad51a624b57efc7e28d4f9fafa9e4bc10
kadeem-lewis <email@kadeemlewis.com>
2024-04-23 05:34:06 -0400

    created double border for footer and navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,17 +4,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="m-auto max-w-md px-4">
-      <header className=" flex items-center justify-between ">
+    <header className="relative mb-1 w-full border-b-4 border-b-primary-fg-muted bg-purple-800 after:pointer-events-none after:absolute after:inset-0 after:-m-2 after:border-b-4 after:border-b-border">
+      <div className="mx-auto flex max-w-md items-center justify-between px-4 py-1">
         <h1 className="text-4xl">
           <Link href="/" className="text-shadow text-5xl font-extrabold">
             POKéDEXLE
           </Link>
         </h1>
-        <div>
-          <ThemeSwitch />
-        </div>
-      </header>
-    </div>
+        <ThemeSwitch />
+      </div>
+    </header>
   );
 }
