05957a06419e12817d26f95ccec42bf0790b1a5e
kadeem-lewis <email@kadeemlewis.com>
2024-04-27 06:40:33 -0400

    combined modal into dialog component and began styling dialog to match theme

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -2,22 +2,49 @@ import React, { ReactNode } from "react";
 import {
   Dialog as AriaDialog,
   DialogProps as AriaDialogProps,
+  DialogTrigger as AriaDialogTrigger,
   Heading,
   Button,
+  ModalOverlay,
+  Modal,
+  ModalOverlayProps,
 } from "react-aria-components";
 import { Icon } from "../Icon";
+import { tv } from "tailwind-variants";
+
+const dialog = tv({
+  slots: {
+    overlay:
+      "fixed left-0 top-0 isolate z-20 flex h-[--visual-viewport-height] w-full items-center justify-center bg-black/[15%] p-4 text-center backdrop-blur-lg",
+    modal:
+      "relative max-h-full w-full max-w-xl rounded-lg border-4 border-white bg-bg-panel bg-clip-padding text-left align-middle text-fg shadow-2xl after:pointer-events-none  after:absolute after:inset-0 after:-m-1.5 after:rounded-lg after:border-4 after:border-border dark:backdrop-blur-2xl dark:backdrop-saturate-200 forced-colors:bg-[Canvas]",
+    content:
+      "relative max-h-[inherit] overflow-auto bg-contain p-6 outline outline-0 before:absolute before:inset-0 before:z-[-1] before:bg-[url('/svgs/pokeball.svg')] before:bg-auto before:bg-center before:bg-no-repeat before:bg-origin-content before:px-4 before:py-1 [[data-placement]>&]:p-4",
+  },
+});
+
+const { overlay, modal, content } = dialog();
+
+function DialogModal({
+  className,
+  ...props
+}: ModalOverlayProps & { className?: string }) {
+  return (
+    <ModalOverlay {...props} isDismissable={true} className={overlay()}>
+      <Modal {...props} className={modal(className)}></Modal>
+    </ModalOverlay>
+  );
+}
 
 interface DialogProps extends Omit<AriaDialogProps, "children"> {
   title: string;
   children: ReactNode;
+  className?: string;
 }
 
-export function Dialog({ title, children, ...props }: DialogProps) {
+function Dialog({ title, children, className, ...props }: DialogProps) {
   return (
-    <AriaDialog
-      {...props}
-      className="relative max-h-[inherit] overflow-auto p-6 outline outline-0 [[data-placement]>&]:p-4"
-    >
+    <AriaDialog {...props} className={content(className)}>
       {({ close }) => (
         <>
           <div className="flex justify-between">
@@ -36,3 +63,7 @@ export function Dialog({ title, children, ...props }: DialogProps) {
     </AriaDialog>
   );
 }
+
+const DialogTrigger = AriaDialogTrigger;
+
+export { DialogModal, Dialog, DialogTrigger };
