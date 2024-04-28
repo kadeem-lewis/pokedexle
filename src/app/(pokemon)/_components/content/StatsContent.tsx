d0fed634bd61ccdd42d991ea4ccba6768c33f0cd
kadeem-lewis <email@kadeemlewis.com>
2024-04-28 06:44:57 -0400

    added placeholder content to stats content

--- src/app/(pokemon)/_components/content/StatsContent.tsx
+++ src/app/(pokemon)/_components/content/StatsContent.tsx
@@ -1,6 +1,34 @@
-"use client" ;
+"use client";
 import React from "react";
 
 export default function StatsContent() {
-  return <div>StatsContent</div>;
+  return (
+    <>
+      <div className="mb-4 grid grid-cols-6 text-center text-xl">
+        <div className="col-span-2">
+          <div>0</div>
+          <div>Played</div>
+        </div>
+        <div className="col-span-2">
+          <div>0</div>
+          <div>Won</div>
+        </div>
+        <div className="col-span-2">
+          <div>0.00</div>
+          <div>Win %</div>
+        </div>
+        <div className="col-span-3">
+          <div>0</div>
+          <div>Current Streak</div>
+        </div>
+        <div className="col-span-3">
+          <div>0</div>
+          <div>Max Streak</div>
+        </div>
+      </div>
+      <div>
+        <p className="text-2xl font-semibold">Guess Distribution</p>
+      </div>
+    </>
+  );
 }
