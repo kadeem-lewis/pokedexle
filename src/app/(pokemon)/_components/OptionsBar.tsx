b61abc06802e7ad70b4747eb817156a874bd9846
kadeem-lewis <email@kadeemlewis.com>
2024-04-28 04:25:22 -0400

    added a support button to options bar

--- src/app/(pokemon)/_components/OptionsBar.tsx
+++ src/app/(pokemon)/_components/OptionsBar.tsx
@@ -7,10 +7,11 @@ import HowToPlayContent from "./content/HowToPlayContent";
 import Calendar from "./content/Calendar";
 import { Button } from "react-aria-components";
 import { Dialog, DialogTrigger, DialogModal } from "@/components/ui/Dialog";
+import Support from "./content/Support";
 
 export default function OptionsBar() {
   return (
-    <nav className=" grid grid-cols-5 border-2 border-foreground">
+    <nav className=" grid grid-cols-6 border-2 border-foreground">
       <DialogTrigger>
         <Button
           className="border-r-2 border-foreground py-1"
@@ -30,16 +31,29 @@ export default function OptionsBar() {
       <DialogTrigger>
         <Button
           className="border-r-2 border-foreground py-1"
-          aria-label="open settings modal"
+          aria-label="open about modal"
         >
           <Icon
-            name="heroicons-cog-6-tooth-solid"
-            className="mx-auto size-6 transition-transform hover:-rotate-45"
+            name="heroicons-exclamation-circle-solid"
+            className="mx-auto h-6 w-6"
           />
         </Button>
         <DialogModal>
-          <Dialog title="Settings">
-            <SettingsContent />
+          <Dialog title="About">
+            <AboutContent />
+          </Dialog>
+        </DialogModal>
+      </DialogTrigger>
+      <DialogTrigger>
+        <Button
+          className="border-r-2 border-foreground py-1"
+          aria-label="open stats modal"
+        >
+          <Icon name="heroicons-chart-bar-solid" className="mx-auto h-6 w-6" />
+        </Button>
+        <DialogModal>
+          <Dialog title="Stats">
+            <StatsContent />
           </Dialog>
         </DialogModal>
       </DialogTrigger>
@@ -62,26 +76,26 @@ export default function OptionsBar() {
       <DialogTrigger>
         <Button
           className="border-r-2 border-foreground py-1"
-          aria-label="open stats modal"
+          aria-label="open support modal"
         >
-          <Icon name="heroicons-chart-bar-solid" className="mx-auto h-6 w-6" />
+          <Icon name="heroicons-heart-solid" className="mx-auto h-6 w-6" />
         </Button>
         <DialogModal>
-          <Dialog title="Stats">
-            <StatsContent />
+          <Dialog title="Support">
+            <Support />
           </Dialog>
         </DialogModal>
       </DialogTrigger>
       <DialogTrigger>
-        <Button className="py-1" aria-label="open about modal">
+        <Button className=" py-1" aria-label="open settings modal">
           <Icon
-            name="heroicons-exclamation-circle-solid"
-            className="mx-auto h-6 w-6"
+            name="heroicons-cog-6-tooth-solid"
+            className="mx-auto size-6 transition-transform hover:-rotate-45"
           />
         </Button>
         <DialogModal>
-          <Dialog title="About">
-            <AboutContent />
+          <Dialog title="Settings">
+            <SettingsContent />
           </Dialog>
         </DialogModal>
       </DialogTrigger>
