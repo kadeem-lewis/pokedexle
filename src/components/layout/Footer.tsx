33acf06fbbe4b0f403b049d041c9c262c14fca00
kadeem-lewis <email@kadeemlewis.com>
2024-04-29 05:08:41 -0400

    updated layout to not have unnecessary scroll and be better laid out

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="relative mt-1 w-full border-t-4 border-t-primary-fg-muted bg-purple-800 text-center after:absolute after:inset-0 after:-m-2 after:border-t-4 after:border-t-border">
+    <footer className="relative mt-1 w-full border-t-4 border-t-primary-fg-muted bg-purple-800 text-center after:absolute after:inset-0 after:-mt-2 after:border-t-4 after:border-t-border">
       <div className="mx-auto max-w-md px-4 py-1">
         <p className=" text-2xl">{year} - pokedexle.com</p>
         <p className="text-xl">
