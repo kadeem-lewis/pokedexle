10f8330cc8015dee28ca60292e1997202a0bdbc5
kadeem-lewis <email@kadeemlewis.com>
2024-04-30 05:32:50 -0400

    updated colors in footer and navbar

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="border-t-fg-accent-muted relative mt-1 w-full border-t-4 bg-purple-800 text-center after:absolute after:inset-0 after:-mt-2 after:border-t-4 after:border-t-border">
+    <footer className="border-t-primary-accent bg-panel-accent-active relative mt-1 w-full border-t-4 text-center after:absolute after:inset-0 after:-mt-2 after:border-t-4 after:border-t-border">
       <div className="mx-auto max-w-md px-4 py-1">
         <p className=" text-2xl">{year} - pokedexle.com</p>
         <p className="text-xl">
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="border-b-fg-accent-muted relative mb-1 w-full border-b-4 bg-purple-800 after:pointer-events-none after:absolute after:inset-0 after:-mb-2 after:border-b-4 after:border-b-border">
+    <header className="border-b-primary-accent bg-panel-accent-active relative mb-1 w-full border-b-4 after:pointer-events-none after:absolute after:inset-0 after:-mb-2 after:border-b-4 after:border-b-border">
       <div className="mx-auto flex max-w-md items-center justify-between px-4 py-1">
         <h1 className="text-4xl">
           <Link href="/" className="text-shadow text-5xl font-extrabold">
