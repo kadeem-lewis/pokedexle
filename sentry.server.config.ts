4629a93cffa28be7f6b37b94445d53eb91075d20
kadeem-lewis <email@kadeemlewis.com>
2026-03-24 04:30:50 -0400

    updated sentry config files

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,15 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://7f3938ab26e510808e8182f3aa25c0c7@o4506349855244288.ingest.us.sentry.io/4507005056581632",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  // Adds request headers and IP for users
+  sendDefaultPii: true,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  // Capture 100% in dev, 10% in production
+  // Adjust based on your traffic volume
+  tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1,
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 });
