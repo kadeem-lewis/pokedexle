6965b47e5edd21e2a34ea431b28840342fb55d19
kadeem-lewis <email@kadeemlewis.com>
2026-03-24 08:05:31 -0400

    updated app to have script for umami analytics

--- src/app/(pokemon)/layout.tsx
+++ src/app/(pokemon)/layout.tsx
@@ -1,6 +1,7 @@
 import OptionsBar from "@/app/(pokemon)/_components/OptionsBar";
 import React, { Suspense } from "react";
 import Loading from "./loading";
+import Script from "next/script";
 
 type PokemonLayoutProps = {
   children: React.ReactNode;
@@ -11,6 +12,11 @@ export default function PokemonLayout({ children }: PokemonLayoutProps) {
     <>
       <OptionsBar />
       <Suspense fallback={<Loading />}>{children}</Suspense>
+      <Script
+        async
+        src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_SRC}
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+      />
     </>
   );
 }
