6a295c90004c095e29d91146cf2261c430055835
kadeem-lewis <email@kadeemlewis.com>
2026-03-24 07:12:27 -0400

    updated prisma generator to use new adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: PrismaClient;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma =
+  globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
