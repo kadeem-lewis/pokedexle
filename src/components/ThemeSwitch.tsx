18b1df95daa445c2c3bf23504149931351c80c8e
kadeem-lewis <email@kadeemlewis.com>
2026-03-24 17:40:40 -0400

    removed unneeded useEffect in ThemeSwitch

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,45 +1,38 @@
 "use client";
-import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { Icon } from "./Icon";
-import { Button, Key } from "react-aria-components";
+import { Button } from "react-aria-components";
 import { MenuContent, MenuItem, MenuTrigger } from "./ui/Menu";
 export default function ThemeSwitch() {
   const { setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
-  const handleThemeChange = (theme: Key) => {
-    setTheme(theme as string);
-  };
 
   return (
     <MenuTrigger>
       <Button
         name="theme-toggle"
         aria-label="Toggle Dark Mode"
-        className="inline-flex transform items-center justify-center rounded-md border-2 border-fg p-1 text-fg transition duration-200 ease-in-out hover:bg-bg-muted"
+        onClick={() => setTheme("light")}
+        className="hover:bg-bg-muted inline-flex transform items-center justify-center rounded-md border-2 border-fg p-1 text-fg transition duration-200 ease-in-out"
       >
         <Icon
           name="pokemon-moon"
-          className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          className="size-6 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
         />
         <Icon
           name="pokemon-sun"
-          className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          className="absolute size-6 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
         />
       </Button>
-      <MenuContent onAction={handleThemeChange} className="text-2xl">
-        <MenuItem id="light">Light</MenuItem>
-        <MenuItem id="dark">Dark</MenuItem>
-        <MenuItem id="system">System</MenuItem>
+      <MenuContent className="text-2xl">
+        <MenuItem id="light" onClick={() => setTheme("light")}>
+          Light
+        </MenuItem>
+        <MenuItem id="dark" onClick={() => setTheme("dark")}>
+          Dark
+        </MenuItem>
+        <MenuItem id="system" onClick={() => setTheme("system")}>
+          System
+        </MenuItem>
       </MenuContent>
     </MenuTrigger>
   );
