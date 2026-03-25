5b338ca6948012aebb09a109b0f25f72ed328706
kadeem-lewis <email@kadeemlewis.com>
2026-03-24 20:04:39 -0400

    finally converted how to play into it's own page

--- src/app/(pokemon)/_components/OptionsBar.tsx
+++ src/app/(pokemon)/_components/OptionsBar.tsx
@@ -1,29 +1,21 @@
 "use client";
 import { Icon } from "@/components/Icon";
-import HowToPlayContent from "./content/HowToPlay";
-import { Button } from "react-aria-components";
-import { Dialog, DialogTrigger, DialogModal } from "@/components/ui/Dialog";
 import Link from "next/link";
 
 export default function OptionsBar() {
   return (
     <nav className="grid grid-cols-6 border-2 border-fg bg-panel ring-4 ring-primary-accent">
-      <DialogTrigger>
-        <Button
-          className="border-r-2 border-fg py-1"
-          aria-label="open how to play modal"
-        >
-          <Icon
-            name="heroicons-question-mark-circle-solid"
-            className="mx-auto size-6"
-          />
-        </Button>
-        <DialogModal>
-          <Dialog title="How to Play">
-            <HowToPlayContent />
-          </Dialog>
-        </DialogModal>
-      </DialogTrigger>
+      <Link
+        href="/how-to-play"
+        className="border-r-2 border-fg py-1"
+        aria-label="open how to play modal"
+      >
+        <Icon
+          name="heroicons-question-mark-circle-solid"
+          className="mx-auto size-6"
+        />
+      </Link>
+
       <Link
         href="/about"
         className="border-r-2 border-fg py-1"
