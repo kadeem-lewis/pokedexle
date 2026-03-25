1446e9a70c201bf7f15018d22fdb926c6efdc5fb
kadeem-lewis <email@kadeemlewis.com>
2026-03-24 21:16:15 -0400

    reverted menu changes

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -4,7 +4,9 @@ import {
   Header as AriaHeader,
   Menu as AriaMenu,
   MenuTrigger as AriaMenuTrigger,
+  Section as AriaSection,
   MenuItem as AriaMenuItem,
+  Popover,
   Separator,
   type MenuItemProps,
   type MenuProps,
@@ -14,7 +16,7 @@ import { tv } from "tailwind-variants";
 export const menu = tv({
   slots: {
     menuPopover:
-      "entering:animate-fade exiting:animate-fadeOut overflow-auto rounded-xl border-2 border-white bg-panel shadow-xl ring-4 ring-border",
+      "entering:animate-fade exiting:animate-fadeOut overflow-auto rounded-xl border-2 border-border bg-panel shadow-xl ring-4 ring-border",
     header: "p-2",
     content: "flex h-fit w-56 flex-col gap-2 p-2 outline-none",
     item: "relative flex cursor-pointer justify-between rounded-md p-2 text-fg outline-none focus:bg-primary",
@@ -22,18 +24,22 @@ export const menu = tv({
   },
 });
 
-const { content, header, item, separator } = menu();
+const { menuPopover, content, header, item, separator } = menu();
 
 const MenuTrigger = AriaMenuTrigger;
+const Section = AriaSection;
 
 const MenuContent = <T extends object>({
   children,
   className,
   ...props
 }: MenuProps<T> & { className?: string }) => (
-  <AriaMenu {...props} className={content({ className })}>
-    {children}
-  </AriaMenu>
+  // @ts-expect-error - This kinda works but popover is no longer used in menu in newer versions
+  <Popover {...props} className={menuPopover()} placement="bottom right">
+    <AriaMenu {...props} className={content({ className })}>
+      {children}
+    </AriaMenu>
+  </Popover>
 );
 
 const MenuItem = ({
@@ -63,4 +69,11 @@ const MenuSeperator = ({
   <Separator {...props} className={separator({ className })} />
 );
 
-export { MenuContent, MenuHeader, MenuItem, MenuSeperator, MenuTrigger };
+export {
+  MenuContent,
+  MenuHeader,
+  MenuItem,
+  MenuSeperator,
+  MenuTrigger,
+  Section,
+};
